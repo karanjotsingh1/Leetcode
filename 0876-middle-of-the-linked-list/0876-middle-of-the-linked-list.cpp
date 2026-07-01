97fdefe8e6d678e21cf5bf30fdbad7923ea31af0
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-01 14:35:03 +0530

    Time: 0 ms (100%), Space: 10 MB (63.97%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -1,27 +1,36 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
+// Approach
+// 1. Pehlaa check kraange je list empty aa ya sirf ik node aa,
+//    ta sidha head return kr devaange.
+// 2. Hun do pointers rakhange:
+//    slow ik step chalega te fast do steps.
+// 3. Jadon fast do steps chal reha hovega,
+//    slow sirf ik step chalega.
+// 4. Jadon fast list de end te pahunch jaave,
+//    slow automatically middle node te hovega.
+// 5. Je nodes even hon,
+//    ta question according second middle node return krna aa.
+// 6. Last ch slow return kr devaange.
+
+class Solution
+{
 public:
-    ListNode* middleNode(ListNode* head) {
-        if(head==NULL || head->next==NULL)
+    ListNode* middleNode(ListNode* head)
+    {
+        if(head==NULL||head->next==NULL)
+        {
             return head;
+        }
 
         ListNode* slow=head;
         ListNode* fast=head;
 
-        while(fast!=NULL && fast->next!=NULL)
+        while(fast!=NULL&&fast->next!=NULL)
         {
+            // Slow moves one step, fast moves two steps
             slow=slow->next;
             fast=fast->next->next;
         }
+
         return slow;
     }
 };
