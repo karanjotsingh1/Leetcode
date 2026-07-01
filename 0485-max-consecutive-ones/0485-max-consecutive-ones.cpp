27e189ec96559b1d66cdb79fc97c6fb6ad420dbd
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-01 16:19:01 +0530

    Time: 0 ms (100%), Space: 50.3 MB (33.33%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -1,13 +1,30 @@
-class Solution {
+// Approach
+// 1. Pehlaa aapaa do variables rakhange:
+//    curr current consecutive 1's count lai
+//    te maxi maximum consecutive 1's lai.
+// 2. Hun array nu left to right traverse kraange.
+// 3. Je current element 1 hove,
+//    ta curr increase kraange te maxi update kraange.
+// 4. Je current element 0 hove,
+//    ta consecutive sequence break ho jaandi aa,
+//    is karke curr nu 0 kr devaange.
+// 5. Last ch maxi hi maximum consecutive 1's houga.
+
+class Solution
+{
 public:
-    int findMaxConsecutiveOnes(vector<int>& nums) {
-        int maxi=0;
+    int findMaxConsecutiveOnes(vector<int>& nums)
+    {
         int curr=0;
+        int maxi=0;
+
         for(int i=0;i<nums.size();i++)
         {
             if(nums[i]==1)
             {
-                curr+=1;
+                curr++;
+
+                // Update the maximum streak of 1's
                 maxi=max(maxi,curr);
             }
             else
@@ -15,6 +32,7 @@ public:
                 curr=0;
             }
         }
+
         return maxi;
     }
 };
