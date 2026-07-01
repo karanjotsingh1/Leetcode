9139ee2210e01a72078fff9c045bf4ad8e7060a7
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-01 14:26:32 +0530

    Time: 3 ms (66.95%), Space: 15.1 MB (12.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,22 +1,39 @@
-#include<map>
-class Solution {
+// Approach
+// 1. Pehlaa aapaa ik map bnaavaange jo value de naal usda index store kru ga.
+// 2. Hun array nu left to right traverse kraange.
+// 3. Har element lai target chon current value subtract krke complement(left_part) kadd laavaange.
+// 4. Je complement pehlaa hi map ch present aa,
+//    matlab pair mil gya te dono indices return kr devaange.
+// 5. Je complement nhi milya,
+//    ta current element te usda index map ch store kr devaange
+//    taaki future ch lookup fast ho sake.
+// 6. Question guarantee krda aa ki ik solution hamesha houga.
+
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
+    vector<int> twoSum(vector<int>& nums,int target)
     {
-        map<int, int> m; // value -> index
+        map<int,int>m;
         vector<int>ans;
-        for (int i=0;i<nums.size();i++) {
-            int left_part=target-nums[i];// left number kdd lo 
-            if (m.find(left_part) != m.end())// if already present in left side of array
-             {
+
+        for(int i=0;i<nums.size();i++)
+        {
+            int left_part=target-nums[i];
+
+            // Check if required complement already exists
+            if(m.find(left_part)!=m.end())
+            {
                 ans.push_back(m[left_part]);
                 ans.push_back(i);
-                
-                return ans; // found the pair
-            }
-            m[nums[i]] = i;// insert this pair also in hash map 
 
+                return ans;
+            }
+
+            // Store current value with its index
+            m[nums[i]]=i;
         }
-        return ans; // no pair found
+
+        return ans;
     }
 };
