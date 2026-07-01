f79405b12ac1cc8777edb03f9cfde122b93c076b
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-01 14:32:04 +0530

    Time: 0 ms (100%), Space: 13.5 MB (41.41%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,29 +1,43 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        if(head==NULL || head->next==NULL)
-            return head;
+// Approach
+// 1. Pehlaa check kraange je list empty aa ya sirf ik node aa,
+//    ta sidha head return kr devaange.
+// 2. Hun tinn pointers rakhange:
+//    prev -> previous node
+//    temp -> current node
+//    next_node -> next node da address store krn lai.
+// 3. Har iteration ch pehlaa next node save kraange,
+//    kyunki next pointer change hon to baad list da agla hissa lose ho sakda aa.
+// 4. Hun current node da next pointer reverse krke prev wal point kraange.
+// 5. Fer prev te temp nu ik step agge move kraange.
+// 6. Jadon temp NULL ho jaave,
+//    prev hi navi reversed list da head houga.
+// 7. Last ch prev return kr devaange.
 
-        ListNode* prev= NULL;
+class Solution
+{
+public:
+    ListNode* reverseList(ListNode* head)
+    {
+        if(head==NULL||head->next==NULL)
+        {
+            return head;
+        }
+
+        ListNode* prev=NULL;
         ListNode* temp=head;
-        
+
         while(temp!=NULL)
         {
+            // Store the next node before breaking the link
             ListNode* next_node=temp->next;
+
+            // Reverse the current link
             temp->next=prev;
+
             prev=temp;
             temp=next_node;
         }
+
         return prev;
     }
 };
