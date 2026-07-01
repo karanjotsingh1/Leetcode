b1455326b73bbf04076ad019b6c33c2508faacee
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-01 14:48:45 +0530

    Time: 0 ms (100%), Space: 77.1 MB (74.87%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,49 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-// class Solution {
-// public:
-//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-//         ListNode* head=new ListNode();
-//         ListNode* temp=head;
-//         int carry=0;
-//         while(l1!=NULL || l2!=NULL || carry){
-//             int sum=0;
-//             if(l1!=NULL){
-//                 sum+=l1->val;
-//                 l1=l1->next;
-//             }
-//             if(l2!=NULL){
-//                 sum+=l2->val;
-//                 l2=l2->next;
-//             }
-//             sum+=carry;
-//             carry=sum/10;
-//             ListNode* newnode=new ListNode(sum%10);
-//             temp->next=newnode;
-//             temp=temp->next;
-//         }
-//         return head->next;
-//     }
-// };
+// Approach
+// 1. Pehlaa aapaa ik dummy node(ans) bnaavaange taaki answer list easily build ho sake.
+// 2. Ik temp pointer rakhange jo hamesha answer list de last node nu point kru ga.
+// 3. Ik carry variable rakhange jo previous addition da carry store kru ga.
+// 4. Hun jadon takk l1, l2 ya carry exist krde ne,
+//    ona nu ik ik karke add kraange.
+// 5. Sum da last digit navi node bna ke answer list ch add kr devaange,
+//    te carry update kr devaange.
+// 6. Jadon saari nodes process ho jaan,
+//    dummy node de next nu return kr devaange.
 
-
-class Solution {
+class Solution
+{
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+    ListNode* addTwoNumbers(ListNode* l1,ListNode* l2)
     {
         ListNode* ans=new ListNode(-1);
         ListNode* temp=ans;
+
         int carry=0;
-        int sum =0;
-        while( l1!=NULL || l2!=NULL || carry)
+        int sum=0;
+
+        while(l1!=NULL||l2!=NULL||carry)
         {
             if(l1!=NULL)
             {
@@ -58,13 +35,16 @@ public:
             }
 
             sum+=carry;
-            carry=(sum>=10)? 1:0;
+            carry=sum/10;
+
+            // Create a node for the current digit
             ListNode* next_node=new ListNode(sum%10);
             temp->next=next_node;
             temp=next_node;
+
             sum=0;
         }
-        
+
         return ans->next;
     }
 };
