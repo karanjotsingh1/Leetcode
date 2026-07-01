88b741149521054dc39a77879762412b56a18c1e
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-01 15:07:00 +0530

    Time: 59 ms (41.82%), Space: 23.9 MB (56.65%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,57 +1,70 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
+// Approach
+// 1. Pehlaa aapaa dono linked lists di length calculate kraange.
+// 2. Hun jis list di length vaddi hougi,
+//    usde pointer nu length difference de barabar agge move kraange.
+// 3. Is naal dove pointers end ton equal distance te aa jaan ge.
+// 4. Hun dove pointers nu ik ik step move kraange.
+// 5. Jadon dono pointers same node nu point krange,
+//    ohi intersection node hougi.
+// 6. Je end takk vi same node na mile,
+//    ta NULL return kr devaange.
+
+class Solution
+{
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode* getIntersectionNode(ListNode* headA,ListNode* headB)
+    {
         int len_a=0;
         int len_b=0;
+
         ListNode* temp1=headA;
         ListNode* temp2=headB;
-        while(temp1)
+
+        while(temp1!=NULL)
         {
-            len_a+=1;
+            len_a++;
             temp1=temp1->next;
         }
-        while(temp2)
+
+        while(temp2!=NULL)
         {
-            len_b+=1;
+            len_b++;
             temp2=temp2->next;
         }
+
         temp1=headA;
         temp2=headB;
 
         int diff=abs(len_a-len_b);
+
+        // Move the pointer of the longer list
         if(len_a>len_b)
         {
-            while(diff)
+            while(diff--)
             {
                 temp1=temp1->next;
-                diff--;
             }
         }
-        else if(len_a<len_b)
+        else
         {
-            while(diff)
+            while(diff--)
             {
                 temp2=temp2->next;
-                diff--;
             }
         }
-        while(temp1 && temp2)
+
+        // Traverse both lists together
+        while(temp1!=NULL&&temp2!=NULL)
         {
-            if(temp1 == temp2)
+            if(temp1==temp2)
             {
                 return temp1;
             }
+
             temp1=temp1->next;
             temp2=temp2->next;
         }
+
         return NULL;
     }
 };
