87021cfcec83c371ba329fc45e0f2d7a3b70a904
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-01 14:59:59 +0530

    Time: 10 ms (36.94%), Space: 12.3 MB (56.07%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -1,21 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
+// Approach
+// 1. Aapaa current node nu directly delete nhi kr sakde,
+//    kyunki previous node da access nhi ditta hoya.
+// 2. Is karke current node ch next node di value copy kr devaange.
+// 3. Fer current node da next pointer,
+//    next node de agge wale node wal point kra devaange.
+// 4. Is naal next node list cho remove ho jaega
+//    te current node di value vi update ho jaegi.
+// 5. Last ch old next node di memory free kr devaange.
+
+class Solution
+{
 public:
-    void deleteNode(ListNode* node) {
-        while(node->next->next!=NULL)
-        {
-            node->val=node->next->val;
-            node=node->next;
-        }
-        node->val=node->next->val;
-        node->next=NULL;
-        return ;
+    void deleteNode(ListNode* node)
+    {
+        ListNode* next_node=node->next;
+
+        // Copy next node's value
+        node->val=next_node->val;
+
+        // Skip the next node
+        node->next=next_node->next;
+
+        delete next_node;
     }
 };
