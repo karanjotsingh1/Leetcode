4acbe9f6e2f17f47c556c4370ca16a2a858b4ac9
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-02 14:06:57 +0530

    Time: 6 ms (56.6%), Space: 13.3 MB (50.77%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,33 +1,47 @@
-class Solution {
+// Approach
+// 1. Pehlaa aapaa Roman symbols di value map ch store kraange.
+// 2. Hun string nu left to right traverse kraange.
+// 3. Har character nu usde next character naal compare kraange.
+// 4. Je current value next value ton chhoti hove,
+//    matlab subtraction case aa (IV, IX, XL...),
+//    ta current value answer cho subtract kr devaange.
+// 5. Nahi ta current value answer ch add kr devaange.
+// 6. Last character da next element hunda nhi,
+//    is karke onu separately answer ch add kraange.
+// 7. Last ch final integer answer return kr devaange.
+
+class Solution
+{
 public:
-    int romanToInt(string s) {
+    int romanToInt(string s)
+    {
         map<char,int>m;
-        m.insert({'I',1});
-        m.insert({'V',5});
-        m.insert({'X',10});
-        m.insert({'L',50});
-        m.insert({'C',100});
-        m.insert({'D',500});
-        m.insert({'M',1000});
+
+        m['I']=1;
+        m['V']=5;
+        m['X']=10;
+        m['L']=50;
+        m['C']=100;
+        m['D']=500;
+        m['M']=1000;
 
         int ans=0;
         int n=s.size();
 
         for(int i=0;i<n-1;i++)
         {
-            char ch=s[i];
-            char ch1=s[i+1];
-            if(m[ch]<m[ch1])
+            if(m[s[i]]<m[s[i+1]])
             {
-                ans-=m[ch];
+                ans-=m[s[i]];
             }
             else
             {
-                ans+=m[ch];
+                ans+=m[s[i]];
             }
         }
+
         ans+=m[s[n-1]];
+
         return ans;
-        
     }
 };
