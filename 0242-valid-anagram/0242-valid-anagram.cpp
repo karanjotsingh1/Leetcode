1b5021e73dc7d2ad30b42bf40d90ffc72ad7854b
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-02 14:23:57 +0530

    Time: 0 ms (100%), Space: 9.9 MB (25.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,47 @@
-class Solution {
+// Approach
+// 1. Pehlaa check kraange je dono strings di length different aa,
+//    ta anagram possible nhi, false return kr devaange.
+// 2. Hun do frequency arrays bnaavaange,
+//    ik s lai te ik t lai.
+// 3. Dono strings traverse krke
+//    har character di frequency count kraange.
+// 4. Hun 26 characters di frequencies compare kraange.
+// 5. Je kise vi character di frequency different hove,
+//    ta false return kr devaange.
+// 6. Je saari frequencies same hon,
+//    ta strings anagram ne, true return kr devaange.
+
+class Solution
+{
 public:
-    bool isAnagram(string s, string t) {
+    bool isAnagram(string s,string t)
+    {
+        if(s.size()!=t.size())
+        {
+            return false;
+        }
+
         vector<int>alpha1(26,0);
         vector<int>alpha2(26,0);
+
         for(int i=0;i<s.size();i++)
         {
-            alpha1[s[i]-'a']+=1;
+            alpha1[s[i]-'a']++;
         }
+
         for(int i=0;i<t.size();i++)
         {
-            alpha2[t[i]-'a']+=1;
+            alpha2[t[i]-'a']++;
         }
+
         for(int i=0;i<26;i++)
         {
             if(alpha1[i]!=alpha2[i])
+            {
                 return false;
+            }
         }
+
         return true;
     }
 };
