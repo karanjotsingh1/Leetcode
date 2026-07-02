614bdf35d8526f8543baf8106df2f312dd387cb5
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-02 14:23:33 +0530

    Time: 4 ms (35.03%), Space: 9.7 MB (49.63%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>alpha1(26,0);
+        vector<int>alpha2(26,0);
+        for(int i=0;i<s.size();i++)
+        {
+            alpha1[s[i]-'a']+=1;
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            alpha2[t[i]-'a']+=1;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(alpha1[i]!=alpha2[i])
+                return false;
+        }
+        return true;
+    }
+};
