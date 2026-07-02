268cebc1158b8577612808b3b2aa58770af75841
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-02 12:38:51 +0530

    Time: 0 ms (100%), Space: 10.6 MB (42.78%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,45 +1,44 @@
-class Solution {
+// Approach
+// 1. Pehlaa poori string reverse kraange.
+// 2. Hun ik ik character traverse kraange.
+// 3. Har word temporary string ch store kraange.
+// 4. Jadon space mile,
+//    ta current word nu reverse krke answer ch add kraange.
+// 5. Multiple spaces automatically ignore ho jaan ge,
+//    kyunki substring empty hove ta add nhi kraange.
+// 6. Last ch extra trailing space remove krke answer return kr devaange.
+
+class Solution
+{
 public:
-    string reverseWords(string s) {
-        int n=s.size();
-
-        int i=0;
-        int j=n-1;
-        while(i<n && s[i]==' ')
-        {
-            i++;
-        }
-        while(j>=0 && s[j]==' ')
-        {
-            j--;
-        }
-
-        
+    string reverseWords(string s)
+    {
         reverse(s.begin(),s.end());
-        s=s+" ";
-        n=s.size();
 
-        string ans;
+        s.push_back(' ');
+
+        string ans="";
         string substring="";
-        
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]==' ' && substring.size()!=0)
-            {
-                reverse(substring.begin(),substring.end());
-                ans+=substring+" ";
-                substring ="";
-            }
 
-            if(s[i]!=' ')
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==' ')
+            {
+                if(substring.size()!=0)
+                {
+                    reverse(substring.begin(),substring.end());
+                    ans+=substring+" ";
+                    substring="";
+                }
+            }
+            else
             {
                 substring+=s[i];
             }
-
-            
         }
-        ans.resize(ans.size()-1);
-        return ans;
 
+        ans.pop_back();
+
+        return ans;
     }
 };
