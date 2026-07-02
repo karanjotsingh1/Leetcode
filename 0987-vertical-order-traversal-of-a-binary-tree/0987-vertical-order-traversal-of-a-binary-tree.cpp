606338b0cf821c452838e1337bcc88bbb6dddada
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-02 15:14:31 +0530

    Time: 0 ms (100%), Space: 15.6 MB (82.96%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,64 +1,82 @@
+// Approach
+// 1. Pehlaa DFS use krke minimum te maximum horizontal distance find kraange.
+// 2. Horizontal range miln to baad,
+//    ona size da answer vector bnaavaange.
+// 3. Hun dubara DFS kraange.
+//    Har node nu usdi horizontal distance de according store kraange.
+//    Pair(level,value) store kraange taaki baad ch sorting ho sake.
+// 4. Har vertical column nu sort kraange.
+//    Pair automatic pehlaa level te fer value de basis te sort hunda aa.
+// 5. Sorted values nu final answer ch copy kr devaange.
+// 6. Last ch vertical traversal return kr devaange.
+
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * struct TreeNode
+ * {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x,TreeNode *left,TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+class Solution
+{
 public:
-
-    void diamter_of_BT(TreeNode* root,int &left,int &ryt,int curr)
+    void findHorizontalRange(TreeNode* root,int& left,int& right,int col)
     {
         if(root==NULL)
+        {
             return;
+        }
 
-        left=min(left,curr);
-        ryt=max(ryt,curr);
-        diamter_of_BT(root->left,left,ryt,curr-1);
-        diamter_of_BT(root->right,left,ryt,curr+1);
+        left=min(left,col);
+        right=max(right,col);
+
+        findHorizontalRange(root->left,left,right,col-1);
+        findHorizontalRange(root->right,left,right,col+1);
     }
 
-    void vertical(TreeNode* root,vector<vector<pair<int,int>>>&ans,int left,int curr,int level)
+    void vertical(TreeNode* root,vector<vector<pair<int,int>>>& ans,int offset,int col,int level)
     {
         if(root==NULL)
+        {
             return;
-        
-        ans[curr+left].push_back({level,root->val});
+        }
 
-        vertical(root->left,ans,left,curr-1,level+1);
-        vertical(root->right,ans,left,curr+1,level+1);
+        ans[col+offset].push_back({level,root->val});
+
+        vertical(root->left,ans,offset,col-1,level+1);
+        vertical(root->right,ans,offset,col+1,level+1);
     }
 
-
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
+    vector<vector<int>> verticalTraversal(TreeNode* root)
+    {
         int left=0;
         int right=0;
-        int curr=0;
-        diamter_of_BT(root,left,right,curr);
-        
-        vector<vector<pair<int,int>>>ans(right-left+1);
-        vector<vector<int>>main_ans(right-left+1);
 
-        left=left*-1;
-        curr=0;
-        int level=0;
-        vertical(root,ans,left,curr,level);
+        findHorizontalRange(root,left,right,0);
 
-        
-        for(int i=0;i<ans.size();i++)
+        vector<vector<pair<int,int>>>temp(right-left+1);
+        vector<vector<int>>ans(right-left+1);
+
+        int offset=-left;
+
+        vertical(root,temp,offset,0,0);
+
+        for(int i=0;i<temp.size();i++)
         {
-             sort(ans[i].begin(), ans[i].end());
-             for(int j=0;j<ans[i].size();j++)
-             {
-                main_ans[i].push_back(ans[i][j].second);
-             }
-        }
-        return main_ans;
+            sort(temp[i].begin(),temp[i].end());
 
+            for(int j=0;j<temp[i].size();j++)
+            {
+                ans[i].push_back(temp[i][j].second);
+            }
+        }
+
+        return ans;
     }
 };
