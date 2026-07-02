dd1c7654223f7a29f0fad14399ca88b776df5a5b
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-02 16:20:55 +0530

    Time: 0 ms (100%), Space: 15.2 MB (16.5%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,70 +1,103 @@
+// Approach
+// 1. Pehlaa do stacks bnaavaange.
+//    Ik current level lai te ik next level lai.
+// 2. Even level te first stack process kraange.
+//    Left child pehlaa push kraange,
+//    fer right child.
+// 3. Odd level te second stack process kraange.
+//    Right child pehlaa push kraange,
+//    fer left child.
+// 4. Is order karke next level automatically
+//    zigzag order ch traverse houga.
+// 5. Har level de values answer ch add kraange.
+// 6. Jadon dono stacks empty ho jaan,
+//    traversal complete ho jaegi.
+
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * struct TreeNode
+ * {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x,TreeNode *left,TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+class Solution
+{
 public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root)
+    {
+        vector<vector<int>>ans;
+
         if(root==NULL)
+        {
             return ans;
+        }
 
         stack<TreeNode*>st1;
         stack<TreeNode*>st2;
-        st1.push(root);
-        int level=0;
-        while(st1.empty()==false || st2.empty()==false)
-        {
-            int n;
-            if(level%2==0)
-                n=st1.size();
-            else
-                n=st2.size();
 
+        st1.push(root);
+
+        int level=0;
+
+        while(!st1.empty()||!st2.empty())
+        {
             vector<int>each_level;
 
-            for(int i=1;i<=n;i++)
+            if(level%2==0)
             {
-                TreeNode* temp;
-                if(level%2==0)
+                int size=st1.size();
+
+                for(int i=0;i<size;i++)
                 {
-                    temp=st1.top();
+                    TreeNode* temp=st1.top();
                     st1.pop();
 
                     each_level.push_back(temp->val);
 
                     if(temp->left)
+                    {
                         st2.push(temp->left);
+                    }
+
                     if(temp->right)
+                    {
                         st2.push(temp->right);
-
-
+                    }
                 }
-                else
+            }
+            else
+            {
+                int size=st2.size();
+
+                for(int i=0;i<size;i++)
                 {
-                    temp=st2.top();
+                    TreeNode* temp=st2.top();
                     st2.pop();
 
                     each_level.push_back(temp->val);
 
                     if(temp->right)
+                    {
                         st1.push(temp->right);
-                    if(temp->left)
-                        st1.push(temp->left);
-                    
-                }
+                    }
 
+                    if(temp->left)
+                    {
+                        st1.push(temp->left);
+                    }
+                }
             }
-            level++;
+
             ans.push_back(each_level);
+            level++;
         }
+
         return ans;
     }
 };
