9434b5c9caa844bbcb45c0964833707a53d407f6
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-03 12:58:11 +0530

    Time: 0 ms (100%), Space: 16.5 MB (21.7%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,50 +1,56 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+// Approach
+// 1. Preorder da pehla element hamesha current subtree da root hunda hai.
+// 2. Har element lai Next Greater Element (NGE) find kraange.
+// 3. NGE da index dassda hai ki left subtree kithe khatam hundi hai
+//    te right subtree kitthon start hundi hai.
+// 4. Root banaun to baad start_ptr+1 ton NGE-1 tak left subtree banaange.
+// 5. NGE ton end_ptr tak right subtree recursively banaange.
+// 6. Je start_ptr end_ptr ton vadda ho jaave,
+//    ta NULL return kar devaange.
+// 7. Is tarah bina preorder nu dubara search kite poori BST build ho jaavegi.
+
+class Solution
+{
 public:
-    TreeNode* make_bst(vector<int>& pre,vector<int>& nge,int n,int start_ptr,int end_ptr)
+    TreeNode* make_bst(vector<int>& pre,vector<int>& nge,int start_ptr,int end_ptr)
     {
         if(start_ptr>end_ptr)
+        {
             return NULL;
+        }
 
         TreeNode* node=new TreeNode(pre[start_ptr]);
 
-        node->left=make_bst(pre,nge,n,start_ptr+1,nge[start_ptr]-1);
-        node->right=make_bst(pre,nge,n,nge[start_ptr],end_ptr);
+        node->left=make_bst(pre,nge,start_ptr+1,nge[start_ptr]-1);
+
+        node->right=make_bst(pre,nge,nge[start_ptr],end_ptr);
 
         return node;
-        
     }
-    TreeNode* bstFromPreorder(vector<int>& pre) {
+
+    TreeNode* bstFromPreorder(vector<int>& pre)
+    {
         int n=pre.size();
+
         if(n==0)
+        {
             return NULL;
+        }
 
         vector<int>nge(n,n);
         stack<int>st;
+
         for(int i=0;i<n;i++)
         {
-            while(st.empty()==false && pre[st.top()]<pre[i])
+            while(st.empty()==false&&pre[st.top()]<pre[i])
             {
                 nge[st.top()]=i;
                 st.pop();
             }
+
             st.push(i);
         }
 
-        int start_ptr=0;
-        int end_ptr=n-1;
-        TreeNode* root=make_bst(pre,nge,n,start_ptr,end_ptr);
-
-        return root;
+        return make_bst(pre,nge,0,n-1);
     }
 };
