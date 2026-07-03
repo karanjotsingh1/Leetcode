d23b0710be62b894614c968b8f10aee74cdcc9fe
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-03 16:20:18 +0530

    Time: 0 ms (100%), Space: 16.9 MB (46.73%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -67,36 +67,28 @@ public:
                 if(row+1<n&&grid[row+1][col]==1)
                 {
                     grid[row+1][col]=2;
-
                     s.erase({row+1,col});
-
                     q.push({row+1,col});
                 }
 
                 if(row-1>=0&&grid[row-1][col]==1)
                 {
                     grid[row-1][col]=2;
-
                     s.erase({row-1,col});
-
                     q.push({row-1,col});
                 }
 
                 if(col+1<m&&grid[row][col+1]==1)
                 {
                     grid[row][col+1]=2;
-
                     s.erase({row,col+1});
-
                     q.push({row,col+1});
                 }
 
                 if(col-1>=0&&grid[row][col-1]==1)
                 {
                     grid[row][col-1]=2;
-
                     s.erase({row,col-1});
-
                     q.push({row,col-1});
                 }
             }
