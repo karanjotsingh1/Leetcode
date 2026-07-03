2a32174fbf8e8328d992724feb1ddb99620647dd
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-03 16:02:07 +0530

    Time: 4 ms (74.15%), Space: 45.1 MB (84.11%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -1,47 +1,61 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
+// Approach
+// 1. Pehla linked list di size calculate kraange.
+// 2. Answer vector nu 0 naal initialize kar devaange.
+// 3. Monotonic decreasing stack use kraange,
+//    jithe (value,index) store hovega.
+// 4. Linked list nu ik vaar traverse kraange.
+// 5. Je current value stack de top ton vaddi hove,
+//    ta current value usda next greater element hovega.
+// 6. Answer update karke stack ton pop kar devaange.
+// 7. Fir current value te usda index stack ch push kar devaange.
+// 8. Jehde elements stack ch bach jaan,
+//    ohna da next greater element exist nahi karda,
+//    is karke answer already 0 hi rahega.
+
+class Solution
+{
 public:
-    vector<int> nextLargerNodes(ListNode* head) {
-        if(head==NULL)  
-            return {};
-
-
-        ListNode* temp1=head;
-        int n=0;
-        while(temp1)
+    vector<int> nextLargerNodes(ListNode* head)
+    {
+        if(head==NULL)
         {
-            n+=1;
-            temp1=temp1->next;
+            return {};
         }
 
-        vector<int> ans(n,0);
-        stack<pair<ListNode*,int>>st;
-        int idx=0;
-        st.push({head,0});
-        ListNode* temp=head->next;
+        int n=0;
+        ListNode* temp=head;
 
-       while(temp!=NULL)
-       {
-        idx+=1;
-            while(st.empty()==false && (st.top()).first->val < temp->val)
+        while(temp!=NULL)
+        {
+            n++;
+
+            temp=temp->next;
+        }
+
+        vector<int>ans(n,0);
+
+        stack<pair<int,int>>st;
+
+        temp=head;
+
+        int idx=0;
+
+        while(temp!=NULL)
+        {
+            while(st.empty()==false&&st.top().first<temp->val)
             {
                 ans[st.top().second]=temp->val;
+
                 st.pop();
             }
 
-            st.push({temp,idx});
+            st.push({temp->val,idx});
+
+            idx++;
+
             temp=temp->next;
-       }
-       
+        }
+
         return ans;
     }
 };
