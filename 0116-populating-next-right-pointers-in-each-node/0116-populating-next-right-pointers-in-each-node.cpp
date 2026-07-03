d3ceb2588b806145290c073598f34e9ddd5fc72d
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-03 11:55:45 +0530

    Time: 15 ms (47.65%), Space: 19.4 MB (25.37%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -1,52 +1,58 @@
-/*
-// Definition for a Node.
-class Node {
+// Approach
+// 1. Je root NULL hove ta sidha NULL return kar devaange.
+// 2. Ik queue use kraange taaki tree nu level by level traverse kar sakiye (BFS).
+// 3. Har level da size store kraange.
+// 4. Hun us level de saare nodes ik ik karke queue cho bahar kadhaange.
+// 5. Je current node level da last node nahi hai,
+//    ta usda next queue de front node naal connect kar devaange.
+// 6. Je current node level da last node hai,
+//    ta usda next NULL set kar devaange.
+// 7. Fir current node de left te right child queue ch push kar devaange.
+// 8. Saare levels process hon to baad root return kar devaange.
+
+class Solution
+{
 public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
-
-class Solution {
-public:
-    Node* connect(Node* root) {
+    Node* connect(Node* root)
+    {
         if(root==NULL)
+        {
             return NULL;
+        }
 
-        queue<Node*> q;
+        queue<Node*>q;
         q.push(root);
+
         while(q.empty()==false)
         {
             int n=q.size();
+
             for(int i=1;i<=n;i++)
             {
-    
                 Node* temp=q.front();
                 q.pop();
-            
+
                 if(i!=n)
+                {
                     temp->next=q.front();
+                }
                 else
+                {
                     temp->next=NULL;
+                }
 
                 if(temp->left)
+                {
                     q.push(temp->left);
-                if(temp->right)
-                    q.push(temp->right);
-                
-            }
-            
-        }
-        return root;
+                }
 
+                if(temp->right)
+                {
+                    q.push(temp->right);
+                }
+            }
+        }
+
+        return root;
     }
 };
