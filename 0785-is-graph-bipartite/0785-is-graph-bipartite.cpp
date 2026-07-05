47acd4c5bc5cc98b770f93c64de5b988487b5f55
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-05 13:37:34 +0530

    Time: 0 ms (100%), Space: 17.7 MB (14.27%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,11 +1,25 @@
-class Solution {
+// Approach
+// 1. Graph disconnected vi ho sakda hai,
+//    is karke har unvisited node ton BFS start kraange.
+// 2. Har node nu do colours (0 te 1) chon ik colour assign kraange.
+// 3. Starting node nu colour 0 assign kar devaange.
+// 4. Current node de saare neighbours nu opposite colour assign kraange.
+// 5. Je neighbour pehla coloured na hove,
+//    ta usnu opposite colour deke queue ch push kar devaange.
+// 6. Je neighbour pehla hi current node de same colour da hove,
+//    ta graph bipartite nahi hai.
+// 7. Je poora graph bina conflict de colour ho jaave,
+//    ta graph bipartite hai.
+
+class Solution
+{
 public:
-    bool colouring(vector<vector<int>>& graph,int n ,vector<int>&colour,vector<bool>&vis,int i)
+    bool colouring(vector<vector<int>>& graph,vector<int>& colour,int start)
     {
         queue<int>q;
-        q.push(i);
-        vis[i]=true;
-        colour[i]=0;
+
+        q.push(start);
+        colour[start]=0;
 
         while(q.empty()==false)
         {
@@ -15,48 +29,39 @@ public:
             for(int i=0;i<graph[node].size();i++)
             {
                 int neigh=graph[node][i];
-                if(vis[neigh]==false)
+
+                if(colour[neigh]==-1)
                 {
-                    if(colour[node]==0)
-                    {
-                        colour[neigh]=1;
-                        vis[neigh]=true;
-                        q.push(neigh);
-                    }
-                    else if(colour[node]==1)
-                    {
-                        colour[neigh]=0;
-                        vis[neigh]=true;
-                        q.push(neigh);
-                    }
+                    colour[neigh]=1-colour[node];
+                    q.push(neigh);
                 }
-                else
+                else if(colour[neigh]==colour[node])
                 {
-                    if(colour[neigh]==colour[node])
-                        return false;
+                    return false;
                 }
             }
-
         }
-        return true;
 
+        return true;
     }
-    bool isBipartite(vector<vector<int>>& graph) {
-        
+
+    bool isBipartite(vector<vector<int>>& graph)
+    {
         int n=graph.size();
-        
+
         vector<int>colour(n,-1);
-        vector<bool>vis(n,false);
 
         for(int i=0;i<n;i++)
         {
-            if(vis[i]==false)
+            if(colour[i]==-1)
             {
-                if(colouring (graph,n,colour,vis,i)==false)
+                if(colouring(graph,colour,i)==false)
+                {
                     return false;
+                }
             }
         }
+
         return true;
-         
     }
 };
