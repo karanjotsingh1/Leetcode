9f334ea0f31a287046ef8fc6de248b8c85525c2b
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-05 14:51:25 +0530

    Time: 5 ms (43.16%), Space: 17.6 MB (96.74%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,58 +1,74 @@
-class Solution {
+// Approach
+// 1. Har prerequisite pair [a,b] da matlab b -> a edge hovegi.
+// 2. Har node di indegree calculate kraange.
+// 3. Jinna nodes di indegree 0 hovegi,
+//    ohna nu queue ch push kar devaange.
+// 4. Queue cho ik ik node bahar kadhaange.
+// 5. Current node nu answer ch add kraange.
+// 6. Current node de saare neighbours di indegree 1 naal ghataange.
+// 7. Je kise neighbour di indegree 0 ho jaave,
+//    ta usnu queue ch push kar devaange.
+// 8. Je answer ch numCourses nodes aa jaan,
+//    ta ohi topological order hovega.
+// 9. Nahi ta cycle exist kardi hai,
+//    is karke empty vector return kar devaange.
+
+class Solution
+{
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> findOrder(int numCourses,vector<vector<int>>& prerequisites)
+    {
         vector<int>adj[numCourses];
 
-        int n=prerequisites.size();
         vector<int>indegree(numCourses,0);
-        for(int i=0;i<n;i++)
-        {
-            int node1=prerequisites[i][0];
-            int node2=prerequisites[i][1];
 
-            indegree[node1]+=1;
-            adj[node2].push_back(node1);
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            int u=prerequisites[i][1];
+            int v=prerequisites[i][0];
+
+            adj[u].push_back(v);
+
+            indegree[v]++;
         }
 
         queue<int>q;
-        for(int i=0;i<indegree.size();i++)
+
+        for(int i=0;i<numCourses;i++)
         {
             if(indegree[i]==0)
+            {
                 q.push(i);
+            }
         }
 
         vector<int>ans;
-        vector<bool>vis(numCourses,false);
-        if(q.size()==0)
-            return ans;
 
         while(q.empty()==false)
         {
             int node=q.front();
             q.pop();
 
-            vis[node]=true;
-
             ans.push_back(node);
 
             for(int i=0;i<adj[node].size();i++)
             {
                 int neigh=adj[node][i];
-                if(vis[neigh]==false)
-                {
-                    indegree[neigh]--;
-                    if(indegree[neigh]==0)
-                        q.push(neigh);
-                }
 
+                indegree[neigh]--;
+
+                if(indegree[neigh]==0)
+                {
+                    q.push(neigh);
+                }
             }
         }
+
         if(ans.size()!=numCourses)
-            ans.clear();
+        {
+            return {};
+        }
+
         return ans;
-
-
-
-
     }
 };
