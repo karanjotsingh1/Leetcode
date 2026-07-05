de58c14905f7cd8d0c78b8f9d4a07724480d2ead
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-05 15:58:06 +0530

    Time: 0 ms (100%), Space: 54.5 MB (44.02%) - LeetHub

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.cpp
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.cpp
@@ -1,23 +1,31 @@
-class Solution {
+// Approach
+// 1. Adjacency matrix di har row traverse kraange.
+// 2. Har row ch kinne 1's ne,
+//    oh count kraange.
+// 3. Row da count hi us vertex di degree hovegi.
+// 4. Degree nu answer vector ch store kar devaange.
+// 5. Aakhir ch answer return kar devaange.
+
+class Solution
+{
 public:
-    vector<int> findDegrees(vector<vector<int>>& matrix) {
-        vector<int>count;
-        int n = matrix.size();
+    vector<int> findDegrees(vector<vector<int>>& matrix)
+    {
+        int n=matrix.size();
 
         vector<int>ans(n,0);
 
-            for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
             {
-                int count=0;
-                for(int j=0;j<n;j++)
+                if(matrix[i][j]==1)
                 {
-                    if(matrix[i][j] == 1)
-                        count+=1;
+                    ans[i]++;
                 }
-                ans[i]=count;
-
             }
-        
+        }
+
         return ans;
     }
 };
