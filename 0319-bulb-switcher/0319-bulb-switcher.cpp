a9df8aec567b083f446d4090ba67789dac494e4c
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-06 16:05:02 +0530

    Time: 0 ms (100%), Space: 7.9 MB (36.42%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,19 +1,16 @@
-#include <cmath>  
 class Solution {
 public:
-    bool isPerfectSquare(long long n) {
-    if (n < 0) return false; // Negative numbers cannot be perfect squares
-    
-    long long root = sqrt(n);
-    return (root * root == n);
-    }
     int bulbSwitch(int n) {
-        int count=0;
-        for(int i=1;i<=n;i++)
+     if(n<=1 && n>=0)  
+     {
+        return n;
+     }
+        int count =0;
+        for(int i=1;i*i<=n;i++) 
         {
-            if(isPerfectSquare(i))
-                count+=1;
+            count++;
         }
         return count;
-    }
+     }
+
 };
