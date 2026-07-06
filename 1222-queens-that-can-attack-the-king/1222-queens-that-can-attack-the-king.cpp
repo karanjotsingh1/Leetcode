d0c66e73c0d9edb2cb48b1af81ac7800ca31db38
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-06 21:46:41 +0530

    Time: 0 ms (100%), Space: 14 MB (89.44%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.cpp
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.cpp
@@ -1,90 +1,40 @@
 class Solution {
 public:
-    void attack(int k_row,int k_col,set<pair<int,int>>s ,vector<vector<int>>&ans)
-    {
-        for(int j=k_col;j>=0;j--)
-        {
-            if(s.find({k_row,j})!=s.end())
-            {
-                ans.push_back({k_row,j});
-                break;
-            }
-        }
-        for(int j=k_col;j<8;j++)
-        {
-            if(s.find({k_row,j})!=s.end())
-            {
-                ans.push_back({k_row,j});
-                break;
-            }
+    vector<vector<int>> queensAttacktheKing(vector<vector<int>>& queens,
+                                            vector<int>& king) {
+
+        bool board[8][8] = {};
+
+        for(auto &q : queens) {
+            board[q[0]][q[1]] = true;
         }
 
-        for(int i=k_row;i>=0;i--)
-        {
-            if(s.find({i,k_col})!=s.end())
-            {
-                ans.push_back({i,k_col});
-                break;
-            }
-        }
-        for(int i=k_row;i<8;i++)
-        {
-            if(s.find({i,k_col})!=s.end())
-            {
-                ans.push_back({i,k_col});
-                break;
-            }
-        }
+        vector<vector<int>> ans;
 
-        for(int i=k_row,j=k_col;i>=0 && j>=0;i--,j--)
-        {
-            if(s.find({i,j})!=s.end())
-            {
-                ans.push_back({i,j});
-                break;
-            }
-        }
-        for(int i=k_row,j=k_col;i>=0 && j<8;i--,j++)
-        {
-            if(s.find({i,j})!=s.end())
-            {
-                ans.push_back({i,j});
-                break;
-            }
-        }
-        for(int i=k_row,j=k_col;i<8 && j>=0;i++,j--)
-        {
-            if(s.find({i,j})!=s.end())
-            {
-                ans.push_back({i,j});
-                break;
-            }
-        }
-        for(int i=k_row,j=k_col;i<8 && j<8;i++,j++)
-        {
-            if(s.find({i,j})!=s.end())
-            {
-                ans.push_back({i,j});
-                break;
-            }
-        }
-        return;
+        int dx[8]={-1,-1,-1, 0, 0, 1, 1, 1};
+        int dy[8]={-1, 0, 1,-1, 1,-1, 0, 1};
 
-    }
-    vector<vector<int>> queensAttacktheKing(vector<vector<int>>& queens, vector<int>& king) {
-        int k_row=king[0];
-        int k_col=king[1];
-        vector<vector<int>>ans;
-
-        set<pair<int,int>>s;
-        for(int i=0;i<queens.size();i++)
+        for(int d = 0; d < 8; d++) 
         {
-            int q_row=queens[i][0];
-            int q_col=queens[i][1];
-            
-            s.insert({q_row,q_col});
+
+            int x = king[0];
+            int y = king[1];
+
+            while(true) {
+
+                x += dx[d];
+                y += dy[d];
+
+                if(x < 0 || x >= 8 || y < 0 || y >= 8)
+                    break;
+
+                if(board[x][y]) {
+
+                    ans.push_back({x, y});
+                    break;
+                }
+            }
         }
-        attack(k_row,k_col,s,ans);
 
         return ans;
     }
