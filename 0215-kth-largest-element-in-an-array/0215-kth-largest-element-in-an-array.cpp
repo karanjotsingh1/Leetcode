3e344c4294dd7f3a334e272427e86dc7eff22fbc
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-08 12:35:52 +0530

    Time: 40 ms (35.04%), Space: 72.5 MB (25.88%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,13 +1,29 @@
-class Solution {
+// Approach
+// 1. Min Heap (Priority Queue) of size k maintain kraange.
+// 2. Array de har element nu heap ch push kraange.
+// 3. Je heap da size k ton vadh ho jaave,
+//    ta smallest element pop kar devaange.
+// 4. Is tarah heap ch hamesha k largest elements hi rahange.
+// 5. Traversal complete hon to baad,
+//    heap da top kth largest element hovega.
+
+class Solution
+{
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for(int i = 0; i < nums.size();i++){
+    int findKthLargest(vector<int>& nums,int k)
+    {
+        priority_queue<int,vector<int>,greater<int>>pq;
+
+        for(int i=0;i<nums.size();i++)
+        {
             pq.push(nums[i]);
-            if(pq.size()>k) pq.pop();
+
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
         }
 
         return pq.top();
-        
     }
 };
