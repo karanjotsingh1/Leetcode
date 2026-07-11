bc555c30db5c5c995d36ebb5f3033573e35e4afd
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-11 21:41:11 +0530

    Time: 58 ms (35.48%), Space: 51.2 MB (9.03%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,27 +1,43 @@
-class Solution {
+// Approach
+// 1. Prefix sum maintain karaange.
+// 2. Ik unordered_map use karaange,
+//    jithe key = prefix sum
+//    te value = us prefix sum di frequency hovegi.
+// 3. Initially map ch (0,1) store karaange,
+//    kyunki prefix sum 0 ik vaar starting ton pehla exist karda hai.
+// 4. Har element te current prefix sum calculate karaange.
+// 5. Required prefix sum = currentPrefix - k calculate karaange.
+// 6. Je eh required prefix sum pehla map ch exist karda hove,
+//    ta usdi frequency answer ch add karaange,
+//    kyunki ohna saare indices ton current index tak
+//    subarray da sum k de equal hovega.
+// 7. Current prefix sum di frequency map ch increase karaange.
+// 8. Saare elements traverse karan to baad
+//    total count return karaange.
+
+class Solution
+{
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-
-        if(n==1 && nums[0]==k)
-            return 1;
-
-        if(n==1 && nums[0]!=k)
-            return 0;
-  
+    int subarraySum(vector<int>& nums,int k)
+    {
         unordered_map<int,int>m;
+
         m[0]=1;
 
         int prefix_sum=0;
         int count=0;
 
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.size();i++)
         {
             prefix_sum+=nums[i];
+
             int remaining=prefix_sum-k;
+
             count+=m[remaining];
-            m[prefix_sum]+=1;
+
+            m[prefix_sum]++;
         }
+
         return count;
     }
 };
