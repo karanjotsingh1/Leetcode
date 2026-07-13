6cacf415176e5092c1ee848232a90083cfe83e69
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 12:56:51 +0530

    Time: 0 ms (100%), Space: 10.1 MB (93.83%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,16 +1,33 @@
-class Solution {
+// Approach
+// 1. Pehla matrix da transpose karaange.
+//    Matlab matrix[i][j] nu matrix[j][i] naal swap karaange.
+// 2. Transpose karan to baad
+//    har row de first column nu
+//    last column naal swap karaange.
+// 3. Fer second column nu
+//    second last column naal swap karaange.
+// 4. Is tarah har row reverse ho jaavegi.
+// 5. Transpose + Reverse rows
+//    milke matrix nu 90° clockwise rotate kar dinde ne.
+// 6. Rotated matrix automatically update ho jaavegi.
+
+class Solution
+{
 public:
-    void rotate(vector<vector<int>>& matrix) {
+    void rotate(vector<vector<int>>& matrix)
+    {
         int n=matrix.size();
 
+        // Transpose the matrix
         for(int i=0;i<n;i++)
         {
             for(int j=i+1;j<n;j++)
             {
                 swap(matrix[i][j],matrix[j][i]);
             }
-        }       
+        }
 
+        // Reverse every row
         int f_col=0;
         int l_col=n-1;
 
@@ -20,9 +37,9 @@ public:
             {
                 swap(matrix[i][f_col],matrix[i][l_col]);
             }
+
             f_col++;
             l_col--;
         }
-        return;
     }
 };
