c8f084c91462473ca0a408a50ffff7e41c2ca526
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 11:06:30 +0530

    Time: 0 ms (100%), Space: 97.4 MB (60.77%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,36 +1,25 @@
-// Approach
-// 1. Pehlaa aapaa buyin_day rakhange jo minimum price wale din da index store kru ga.
-// 2. Hun array nu left to right traverse kraange.
-// 3. Je current price buyin_day wale price ton chhoti aa,
-//    ta buyin_day update kr devaange kyunki sanu hor sasta stock mil gya.
-// 4. Nahi ta current day te sell krke profit calculate kraange.
-// 5. Har step te maximum profit update krde jaavaange.
-// 6. Je kise vi transaction naal profit na bane,
-//    ta profit 0 hi return houga.
-
-class Solution
-{
+class Solution {
 public:
-    int maxProfit(vector<int>& prices)
-    {
+    int maxProfit(vector<int>& prices) {
+        int maxi=0;
         int n=prices.size();
-        int buyin_day=0;
-        int profit=0;
 
-        for(int i=0;i<n;i++)
+        if(n==1)
+            return 0;
+
+        int best_buy=0;
+
+        for(int i=1;i<n;i++)
         {
-            // Update the day having minimum buying price
-            if(prices[i]<prices[buyin_day])
+            if(prices[i]<prices[best_buy])
             {
-                buyin_day=i;
+                best_buy=i;
             }
             else
             {
-                // Calculate profit by selling today
-                profit=max(profit,prices[i]-prices[buyin_day]);
+                maxi=max(maxi,prices[i]-prices[best_buy]);
             }
         }
-
-        return profit;
+        return maxi;
     }
 };
