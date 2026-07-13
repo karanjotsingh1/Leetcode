6fb8cdd1bd7592dfed3989cce4667b551d893840
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 11:38:07 +0530

    Time: 0 ms (100%), Space: 62.8 MB (98.29%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,48 +1,33 @@
-// Approach
-// 1. Do pointers use karaange:
-//    ik array de starting te (left)
-//    te ik ending te (right).
-// 2. Har step te current container da area calculate karaange.
-//    Area = min(height[left],height[right]) × (right-left).
-// 3. Maximum area update karaange.
-// 4. Jis side di height chhoti hove,
-//    us pointer nu move karaange,
-//    kyunki area nu improve karan da chance
-//    sirf chhoti height nu increase karan naal hunda hai.
-// 5. Je dono heights equal hon,
-//    ta kise vi ik pointer nu move kar sakde haan.
-// 6. Jadon left pointer right pointer nu meet kar jaave,
-//    traversal complete ho jaavegi.
-// 7. Maximum area return karaange.
-
-class Solution
-{
+class Solution {
 public:
-    int maxArea(vector<int>& height)
-    {
+    int maxArea(vector<int>& height) {
         int n=height.size();
 
         int left=0;
-        int right=n-1;
+        int ryt=n-1;
 
-        int max_area=0;
+        int maxi=0;
 
-        while(left<right)
+        while(left<ryt)
         {
-            int area=min(height[left],height[right])*(right-left);
+            int horizontal=ryt-left;
+            int vertical=min(height[left],height[ryt]);
+            maxi=max(maxi,horizontal*vertical);
 
-            max_area=max(max_area,area);
-
-            if(height[left]<height[right])
+            if(height[left] < height[ryt])
             {
                 left++;
             }
+            else if(height[left] > height[ryt])
+            {
+                ryt--;
+            }
             else
             {
-                right--;
+                left++;
+                ryt--;
             }
         }
-
-        return max_area;
+        return maxi;
     }
 };
