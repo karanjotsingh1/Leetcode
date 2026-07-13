6257aa9a736f9377d846281e34c67633baa95562
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 10:55:09 +0530

    Time: 0 ms (100%), Space: 11.6 MB (62.28%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,12 +1,29 @@
-class Solution {
+// Approach
+// 1. Last digit ch 1 add karaange.
+// 2. Je last digit 10 ban jaave,
+//    ta usnu 0 bana ke carry generate karaange.
+// 3. Carry nu left side propagate karaange.
+// 4. Jadon tak carry hove,
+//    har digit check karaange.
+// 5. Je digit 9 hove,
+//    ta usnu 0 bana ke carry continue rakhaange.
+// 6. Je digit 9 na hove,
+//    ta usnu 1 increase karaange
+//    te carry khatam kar devaange.
+// 7. Je saari digits process hon to baad vi carry bach jaave,
+//    ta beginning ch 1 insert karaange.
+// 8. Updated array return karaange.
+
+class Solution
+{
 public:
-    vector<int> plusOne(vector<int>& digits) {
+    vector<int> plusOne(vector<int>& digits)
+    {
         int n=digits.size();
 
-        
+        digits[n-1]++;
 
-        digits[n-1]+=1;
-        int carry =0;
+        int carry=0;
 
         if(digits[n-1]==10)
         {
@@ -24,10 +41,10 @@ public:
             }
             else
             {
-                digits[idx]+=1;
+                digits[idx]++;
                 carry=0;
-                break;
             }
+
             idx--;
         }
 
