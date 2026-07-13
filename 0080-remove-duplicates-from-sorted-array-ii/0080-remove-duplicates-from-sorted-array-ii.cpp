fb21d06523c8510d97a7caa65a0e6c95ffb93f86
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 15:34:59 +0530

    Time: 4 ms (84.25%), Space: 19.6 MB (24.61%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,27 +1,46 @@
-class Solution {
+// Approach
+// 1. Array sorted hai,
+//    is karke duplicate elements continuously aunge.
+// 2. Array nu ik vaar traverse karaange.
+// 3. Har unique element di frequency count karaange.
+// 4. Je frequency 2 ya us ton vadh hove,
+//    ta us element nu answer array ch sirf 2 vaar store karaange.
+// 5. Je frequency 1 hove,
+//    ta usnu ik vaar store karaange.
+// 6. Ik pointer (idx) maintain karaange,
+//    jo next valid position dasega.
+// 7. Traversal complete hon to baad
+//    idx hi modified array di length hovegi.
+
+class Solution
+{
 public:
-    int removeDuplicates(vector<int>& nums) {
+    int removeDuplicates(vector<int>& nums)
+    {
         int n=nums.size();
+
         int idx=0;
 
         for(int i=0;i<n;i++)
         {
             int ele=nums[i];
+
             int count=1;
-            while(i+1 <n && nums[i+1]==nums[i])
+
+            while(i+1<n && nums[i+1]==ele)
             {
-                count+=1;
+                count++;
                 i++;
             }
-            
+
             if(count>=2)
             {
-                nums[idx++]=nums[i];
-                nums[idx++]=nums[i];
+                nums[idx++]=ele;
+                nums[idx++]=ele;
             }
-            else if(count==1)
+            else
             {
-                nums[idx++]=nums[i];
+                nums[idx++]=ele;
             }
         }
 
