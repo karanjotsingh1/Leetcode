e687805ae8bc571ce8e95b1341a77f855c88df8a
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 21:22:58 +0530

    Time: 0 ms (100%), Space: 16.1 MB (69.57%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -1,34 +1,42 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
+// Approach
+// 1. Ik pointer (temp) head ton start karaange.
+// 2. Har node lai usdi value store karaange.
+// 3. Ik hor pointer (temp1) current node nu point karega.
+// 4. Agge traverse karaange
+//    jadon tak duplicate values mil diya ne.
+// 5. Jadon next different value mil jaave
+//    ya list end ho jaave,
+//    ta temp1->next nu us different node naal connect kar devaange.
+// 6. Is tarah saare duplicate nodes skip ho jaan ge.
+// 7. Traversal complete hon to baad
+//    modified linked list da head return karaange.
+
+class Solution
+{
 public:
-    ListNode* deleteDuplicates(ListNode* head) {
+    ListNode* deleteDuplicates(ListNode* head)
+    {
         if(head==NULL || head->next==NULL)
+        {
             return head;
+        }
 
         ListNode* temp=head;
 
         while(temp!=NULL)
         {
             int val=temp->val;
+
             ListNode* temp1=temp;
 
             temp=temp->next;
+
             while(temp!=NULL && temp->val==val)
             {
                 temp=temp->next;
             }
 
             temp1->next=temp;
-
         }
 
         return head;
