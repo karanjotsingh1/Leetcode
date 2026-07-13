f56dbdb858b21952ba535825fa45bc69f283ab8f
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 11:09:31 +0530

    Time: 1 ms (24.34%), Space: 20.7 MB (58.98%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -5,8 +5,8 @@ public:
         if(n==1)
             return nums[0];
 
-        int x_o_r=nums[0];
-        for(int i=1;i<n;i++)
+        int x_o_r=0;
+        for(int i=0;i<n;i++)
         {
             x_o_r=x_o_r ^ nums[i];
         }
