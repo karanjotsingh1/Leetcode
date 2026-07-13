a2a2bfd32d1b1b7a73f976bae5eeaa439ea42e9f
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 11:06:50 +0530

    Time: 0 ms (100%), Space: 97.4 MB (25.3%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,30 @@
-class Solution {
+// Approach
+// 1. Ik variable maintain karaange
+//    jo sab ton sasti buying day (best_buy) nu store karega.
+// 2. Array nu left to right traverse karaange.
+// 3. Je current price best_buy ton chhoti hove,
+//    ta best_buy update karaange.
+// 4. Nahi ta current day te sell karan da profit calculate karaange.
+// 5. Maximum profit nu continuously update karaange.
+// 6. Traversal complete hon to baad
+//    maximum profit return karaange.
+// 7. Je koi profitable transaction possible na hove,
+//    ta answer 0 hi rahega.
+
+class Solution
+{
 public:
-    int maxProfit(vector<int>& prices) {
-        int maxi=0;
+    int maxProfit(vector<int>& prices)
+    {
         int n=prices.size();
 
         if(n==1)
+        {
             return 0;
+        }
 
         int best_buy=0;
+        int max_profit=0;
 
         for(int i=1;i<n;i++)
         {
@@ -17,9 +34,10 @@ public:
             }
             else
             {
-                maxi=max(maxi,prices[i]-prices[best_buy]);
+                max_profit=max(max_profit,prices[i]-prices[best_buy]);
             }
         }
-        return maxi;
+
+        return max_profit;
     }
 };
