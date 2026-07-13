c399c0d4b3d3a0aa840afa8948afd41dfbf8c5c3
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 14:45:33 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.4%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,54 +1,77 @@
-class Solution {
+// Approach
+// 1. Pehla Binary Search use karke
+//    oh row find karaange
+//    jisde range ch target aa sakda hai.
+// 2. Je aisi koi row na mile,
+//    ta false return karaange.
+// 3. Je row mil jaave,
+//    ta us row te Binary Search lagaange.
+// 4. Je target mil jaave,
+//    ta true return karaange.
+// 5. Je Binary Search complete ho jaave
+//    te target na mile,
+//    ta false return karaange.
+
+class Solution
+{
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    bool searchMatrix(vector<vector<int>>& matrix,int target)
+    {
         int n=matrix.size();
         int m=matrix[0].size();
 
-        // row searching 
         int up=0;
         int down=n-1;
-        
-        int mid;
+
+        int row=-1;
+
+        // Find the correct row
         while(up<=down)
         {
-            mid=up+(down-up)/2;
+            int mid=up+(down-up)/2;
 
-            if(matrix[mid][0] > target)
+            if(matrix[mid][0]>target)
             {
                 down=mid-1;
             }
-            else if(matrix[mid][m-1] < target)
+            else if(matrix[mid][m-1]<target)
             {
                 up=mid+1;
             }
             else
             {
+                row=mid;
                 break;
             }
+        }
+
+        if(row==-1)
+        {
+            return false;
         }
 
         int left=0;
         int ryt=m-1;
 
+        // Binary Search in the row
         while(left<=ryt)
         {
-            int mid_col=left+(ryt-left)/2;
+            int mid=left+(ryt-left)/2;
 
-            if(matrix[mid][mid_col]==target)
+            if(matrix[row][mid]==target)
             {
                 return true;
             }
-            else if(matrix[mid][mid_col]< target)
+            else if(matrix[row][mid]<target)
             {
-                left=mid_col+1;
+                left=mid+1;
             }
             else
             {
-                ryt=mid_col-1;
+                ryt=mid-1;
             }
         }
+
         return false;
-        
-        
     }
 };
