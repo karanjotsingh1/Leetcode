8c8e1a8fabc040a8f8dc597e7a96136f4396cdeb
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 14:34:43 +0530

    Time: 35 ms (6.62%), Space: 24.1 MB (5.31%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,56 +1,37 @@
-// Step 1:
-// Traverse the entire matrix.
-
-// Step 2:
-// Jithe vi 0 mile,
-// usdi row number ik set ch store kr devaange.
-// usda column number ik hor set ch store kr devaange.
-
-// Step 3:
-// Hun dubara poori matrix traverse kraange.
-
-// Step 4:
-// Je current row set ch present aa
-// ya current column set ch present aa,
-// taan us cell nu 0 kr devaange.
-
-// Step 5:
-// Finally updated matrix mil jaegi.
-
-class Solution
-{
+class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix)
-    {
-        int m=matrix.size();
-        int n=matrix[0].size();
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        int n=matrix.size();
+        int m=matrix[0].size();
+        set<pair<int,int>>s;
 
-        unordered_set<int>rows;
-        unordered_set<int>cols;
-
-        // Store rows and columns containing 0
-        for(int i=0;i<m;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    rows.insert(i);
-                    cols.insert(j);
+                    s.insert({i,j});
                 }
             }
         }
 
-        // Set required cells to 0
-        for(int i=0;i<m;i++)
+        for(auto pos: s)
         {
-            for(int j=0;j<n;j++)
+            int row=pos.first;
+            int col=pos.second;
+
+            for(int i=0;i<m;i++)
             {
-                if(rows.count(i)||cols.count(j))
-                {
-                    matrix[i][j]=0;
-                }
+                matrix[row][i]=0;
             }
+            for(int i=0;i<n;i++)
+            {
+                matrix[i][col]=0;
+            }
+
         }
+        return;
     }
 };
