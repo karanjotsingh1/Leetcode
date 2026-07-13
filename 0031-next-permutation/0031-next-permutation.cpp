6de12a26a98c249d80dff5343eee3789dc4460d9
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 12:02:54 +0530

    Time: 0 ms (100%), Space: 15.6 MB (72.4%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,13 +1,35 @@
-class Solution {
+// Approach
+// 1. Right to left traverse karke
+//    pehla aisa index (pivot) find karaange
+//    jithe nums[i] < nums[i+1] hove.
+// 2. Je aisa koi pivot na mile,
+//    matlab array descending order ch hai,
+//    ta smallest permutation banaan lai
+//    poori array reverse (ya sort) kar devaange.
+// 3. Je pivot mil jaave,
+//    ta right side ton pehla aisa element find karaange
+//    jo pivot element ton vadda hove.
+// 4. Pivot te us element nu swap karaange.
+// 5. Pivot de right wala part descending order ch hunda hai,
+//    is karke usnu reverse kar devaange,
+//    taaki smallest possible permutation mil jaave.
+// 6. Updated array hi next permutation hovegi.
+
+class Solution
+{
 public:
-    void nextPermutation(vector<int>& nums) {
+    void nextPermutation(vector<int>& nums)
+    {
         int n=nums.size();
 
         if(n==0)
+        {
             return;
-        
+        }
+
         int pivot=-1;
 
+        // Find pivot
         for(int i=n-2;i>=0;i--)
         {
             if(nums[i]<nums[i+1])
@@ -17,13 +39,15 @@ public:
             }
         }
 
+        // Last permutation
         if(pivot==-1)
         {
-            sort(nums.begin(),nums.end());
+            reverse(nums.begin(),nums.end());
             return;
         }
 
-        for(int i=n-1;i>=0;i--)
+        // Find next greater element
+        for(int i=n-1;i>pivot;i--)
         {
             if(nums[i]>nums[pivot])
             {
@@ -32,6 +56,7 @@ public:
             }
         }
 
+        // Reverse the suffix
         int left=pivot+1;
         int ryt=n-1;
 
@@ -41,7 +66,5 @@ public:
             left++;
             ryt--;
         }
-
-        return;
     }
 };
