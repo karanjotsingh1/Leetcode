40f536b0addce904644a90e80fa3ccefeb97e000
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 11:23:57 +0530

    Time: 0 ms (100%), Space: 21.9 MB (38.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,20 +1,33 @@
-class Solution {
+// Approach
+// 1. Array ch numbers 0 to n tak hone chahide ne,
+//    jithe ik number missing hai.
+// 2. 1 to n tak da expected sum calculate karaange.
+//    (0 add karan naal koi difference nahi painda.)
+// 3. Array de saare elements da actual sum calculate karaange.
+// 4. Expected sum chon actual sum subtract karaange.
+// 5. Remaining value hi missing number hovegi.
+
+class Solution
+{
 public:
-    int missingNumber(vector<int>& nums) {
-        
+    int missingNumber(vector<int>& nums)
+    {
         int n=nums.size();
 
-        int sum=0;
+        int expected_sum=0;
+
         for(int i=1;i<=n;i++)
         {
-            sum+=i;
+            expected_sum+=i;
         }
 
-        int arr_sum=0;
+        int actual_sum=0;
+
         for(int i=0;i<n;i++)
         {
-            arr_sum+=nums[i];
+            actual_sum+=nums[i];
         }
-        return sum-arr_sum;
+
+        return expected_sum-actual_sum;
     }
 };
