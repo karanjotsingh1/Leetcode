d327f5067bef6eb77210e8990e3bff0cae3eae5c
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 10:36:51 +0530

    Time: 0 ms (100%), Space: 11.8 MB (48.81%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,13 +1,30 @@
-class Solution {
+// Approach
+// 1. Ik pointer (idx) maintain karaange,
+//    jo next valid element di position represent karega.
+// 2. Array nu ik vaar traverse karaange.
+// 3. Je current element val de equal na hove,
+//    ta usnu idx position te store karaange.
+// 4. Fer idx nu increment karaange.
+// 5. Je current element val de equal hove,
+//    ta usnu ignore karaange.
+// 6. Traversal complete hon to baad,
+//    idx hi remaining valid elements di count hovegi.
+// 7. idx return karaange.
+
+class Solution
+{
 public:
-    int removeElement(vector<int>& nums, int val) {
-        
+    int removeElement(vector<int>& nums,int val)
+    {
         int n=nums.size();
-        
-        if(n==0)    
+
+        if(n==0)
+        {
             return 0;
+        }
 
         int idx=0;
+
         for(int i=0;i<n;i++)
         {
             if(nums[i]!=val)
