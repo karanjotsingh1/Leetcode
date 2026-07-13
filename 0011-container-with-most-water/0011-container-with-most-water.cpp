b581f8e40d1fed4a427a2a4b4e4da1cdb555740b
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 11:39:02 +0530

    Time: 0 ms (100%), Space: 62.9 MB (49.08%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,6 +1,28 @@
-class Solution {
+// Approach
+// 1. Do pointers use karaange:
+//    left array de starting te
+//    te ryt array de ending te.
+// 2. Har step te container di width calculate karaange.
+//    Width = ryt-left.
+// 3. Container di height
+//    left te ryt di minimum height hovegi.
+// 4. Area = width × minimum height calculate karaange.
+// 5. Maximum area (maxi) update karaange.
+// 6. Jis side di height chhoti hove,
+//    us pointer nu move karaange,
+//    kyunki area nu improve karan da chance
+//    sirf chhoti height nu increase karan naal hunda hai.
+// 7. Je dono heights equal hon,
+//    ta kise vi ik pointer nu move kar sakde haan.
+// 8. Jadon left te ryt mil jaan,
+//    traversal complete ho jaavegi.
+// 9. Maximum area return karaange.
+
+class Solution
+{
 public:
-    int maxArea(vector<int>& height) {
+    int maxArea(vector<int>& height)
+    {
         int n=height.size();
 
         int left=0;
@@ -11,14 +33,16 @@ public:
         while(left<ryt)
         {
             int horizontal=ryt-left;
+
             int vertical=min(height[left],height[ryt]);
+
             maxi=max(maxi,horizontal*vertical);
 
-            if(height[left] < height[ryt])
+            if(height[left]<height[ryt])
             {
                 left++;
             }
-            else if(height[left] > height[ryt])
+            else if(height[left]>height[ryt])
             {
                 ryt--;
             }
@@ -28,6 +52,7 @@ public:
                 ryt--;
             }
         }
+
         return maxi;
     }
 };
