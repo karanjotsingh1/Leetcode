8ff3f55c4489d315bc8bc5852aadc299b05baec1
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 21:49:18 +0530

    Time: 0 ms (100%), Space: 118.1 MB (65.51%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,43 +1,28 @@
-// Approach
-// 1. Pehlaa slow te fast pointer use krke linked list da middle labhaange.
-// 2. Hun middle to baad wali half nu reverse kr devaange.
-// 3. Ik pointer head ton te ik pointer reversed second half ton start kraange.
-// 4. Dono lists nu ik ik node compare kraange.
-// 5. Je kise vi node di value different hove,
-//    ta false return kr devaange.
-// 6. Je poori second half successfully compare ho jaave,
-//    ta linked list palindrome aa, true return kr devaange.
-
 /**
  * Definition for singly-linked list.
- * struct ListNode
- * {
+ * struct ListNode {
  *     int val;
  *     ListNode *next;
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x,ListNode *next) : val(x), next(next) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-class Solution
-{
+class Solution {
 public:
-    bool isPalindrome(ListNode* head)
-    {
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL || head->next==NULL)
+            return true;
+
         ListNode* slow=head;
         ListNode* fast=head;
-
-        // Find the middle of the linked list
-        while(fast!=NULL&&fast->next!=NULL)
+        while(fast!=NULL && fast->next!=NULL)
         {
             slow=slow->next;
             fast=fast->next->next;
         }
 
         ListNode* prev=NULL;
-
-        // Reverse the second half
         while(slow!=NULL)
         {
             ListNode* next_node=slow->next;
@@ -47,19 +32,14 @@ public:
         }
 
         fast=head;
-
-        // Compare first half with reversed second half
-        while(fast!=NULL&&prev!=NULL)
+        while(fast!=NULL && prev!=NULL)
         {
             if(fast->val!=prev->val)
-            {
                 return false;
-            }
 
-            fast=fast->next;
             prev=prev->next;
+            fast=fast->next;
         }
-
         return true;
     }
 };
