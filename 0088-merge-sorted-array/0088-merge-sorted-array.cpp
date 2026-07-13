f9931a1d2e1afc4011eafa535a6d123ac5eb8cb7
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 11:00:37 +0530

    Time: 0 ms (100%), Space: 12.6 MB (9.62%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,44 +1,50 @@
-class Solution {
+// Approach
+// 1. Do pointers use karaange:
+//    ptr1 nums1 de valid elements de end te,
+//    ptr2 nums2 de end te.
+// 2. Ik third pointer idx
+//    nums1 de last index te rakhaange.
+// 3. Dono arrays de last elements compare karaange.
+// 4. Jeda element vadda hove,
+//    usnu nums1[idx] te place karaange.
+// 5. Corresponding pointer te idx nu decrease karaange.
+// 6. Jadon kise ik array de elements khatam ho jaan,
+//    ta nums2 de remaining elements nums1 ch copy karaange.
+// 7. Je nums1 de elements bach jaan,
+//    unha nu move karan di zaroorat nahi,
+//    kyunki oh pehla hi correct position te ne.
+
+class Solution
+{
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
+    void merge(vector<int>& nums1,int m,vector<int>& nums2,int n)
+    {
         int ptr1=m-1;
         int ptr2=n-1;
 
-        int idx=nums1.size()-1;
+        int idx=m+n-1;
 
         while(ptr1>=0 && ptr2>=0)
         {
             if(nums1[ptr1]>nums2[ptr2])
             {
                 nums1[idx]=nums1[ptr1];
-                idx--;
                 ptr1--;
-            }
-            else if(nums1[ptr1]<nums2[ptr2])
-            {
-                nums1[idx]=nums2[ptr2];
-                idx--;
-                ptr2--;
             }
             else
             {
                 nums1[idx]=nums2[ptr2];
-                idx--;
                 ptr2--;
-
-                nums1[idx]=nums1[ptr1];
-                idx--;
-                ptr1--;
             }
+
+            idx--;
         }
 
         while(ptr2>=0)
         {
             nums1[idx]=nums2[ptr2];
-            idx--;
             ptr2--;
+            idx--;
         }
-        return;
     }
 };
