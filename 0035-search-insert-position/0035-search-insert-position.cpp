733d7045abeac46b8f157018dd2fc7c0ff9e80b6
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 10:48:02 +0530

    Time: 0 ms (100%), Space: 13.6 MB (78.12%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,41 +1,28 @@
-// Approach
-// 1. Binary Search use kraange.
-// 2. Je target middle element de equal hove,
-//    ta usda index return kar devaange.
-// 3. Je target middle element ton vadda hove,
-//    ta right half ch search kraange.
-// 4. Je target middle element ton chhota hove,
-//    ta left half ch search kraange.
-// 5. Jadon loop khatam hove,
-//    ta left hi target di correct insertion position hovegi.
-
-class Solution
-{
+class Solution {
 public:
-    int searchInsert(vector<int>& nums,int target)
-    {
+    int searchInsert(vector<int>& arr, int target) {
+        int n=arr.size();
+
         int left=0;
-        int right=nums.size()-1;
+        int ryt=n-1;
 
-        while(left<=right)
+        while(left<=ryt)
         {
-            int mid=left+(right-left)/2;
-
-            if(nums[mid]==target)
-            {
-                return mid;
-            }
-
-            if(nums[mid]<target)
+            int mid=(left+((ryt-left)/2));
+            
+            if(arr[mid] < target)
             {
                 left=mid+1;
             }
+            else if(arr[mid] > target)
+            {
+                ryt=mid-1;
+            }
             else
             {
-                right=mid-1;
+                return mid;
             }
         }
-
         return left;
     }
 };
