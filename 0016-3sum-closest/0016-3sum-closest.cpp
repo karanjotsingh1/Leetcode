f14aeae70965018f8ace394487bd0417e6e24d6b
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 11:55:34 +0530

    Time: 15 ms (31.07%), Space: 14 MB (78.69%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -28,9 +28,7 @@ public:
     int threeSumClosest(vector<int>& nums,int target)
     {
         sort(nums.begin(),nums.end());
-
         int n=nums.size();
-
         int closest=nums[0]+nums[1]+nums[2];
 
         for(int i=0;i<n-2;i++)
