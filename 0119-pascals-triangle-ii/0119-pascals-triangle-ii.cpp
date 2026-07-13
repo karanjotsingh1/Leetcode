69f488a7f7a50ae95b6ce1dbe91b988ef574d2ed
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 11:03:06 +0530

    Time: 0 ms (100%), Space: 9 MB (33.03%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,22 +1,47 @@
-class Solution {
+// Approach
+// 1. First row [1] ton start karaange.
+// 2. Previous row nu store karan lai ik vector maintain karaange.
+// 3. Har navi row di starting te 1 add karaange.
+// 4. Previous row de adjacent elements da sum
+//    current row ch add karaange.
+// 5. Current row de end te vi 1 add karaange.
+// 6. Current row nu previous row bana ke
+//    aggli iteration lai use karaange.
+// 7. Jadon required row ban jaave,
+//    usnu answer vich return karaange.
+
+class Solution
+{
 public:
-    vector<int> getRow(int rowIndex) {
-        vector<int>ans;
+    vector<int> getRow(int rowIndex)
+    {
         vector<int> prev;
+
         prev.push_back(1);
+
         if(rowIndex==0)
+        {
             return prev;
+        }
+
+        vector<int> ans;
+
         for(int i=0;i<rowIndex;i++)
         {
-            ans.resize(0);
+            ans.clear();
+
             ans.push_back(1);
+
             for(int j=1;j<prev.size();j++)
             {
-                ans.push_back(prev[j] + prev[j-1]);
+                ans.push_back(prev[j-1]+prev[j]);
             }
+
             ans.push_back(1);
+
             prev=ans;
         }
+
         return ans;
     }
 };
