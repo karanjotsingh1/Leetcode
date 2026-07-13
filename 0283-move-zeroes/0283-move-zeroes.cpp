7402e28644290d314751be6ed697e8f40da02b67
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-13 11:30:50 +0530

    Time: 0 ms (100%), Space: 23.8 MB (56.17%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,14 +1,36 @@
-class Solution {
+// Approach
+// 1. Pehla array ch total zeroes count karaange.
+// 2. Ik pointer (idx) maintain karaange,
+//    jo next non-zero element di position represent karega.
+// 3. Array nu ik vaar traverse karaange.
+// 4. Je current element non-zero hove,
+//    ta usnu idx position te place karaange
+//    te idx nu increment karaange.
+// 5. Saare non-zero elements place hon to baad,
+//    remaining positions te zero fill karaange.
+// 6. Is tarah saare non-zero elements
+//    apni relative order ch rehnde ne
+//    te saare zeroes end ch move ho jaande ne.
+
+class Solution
+{
 public:
-    void moveZeroes(vector<int>& nums) {
+    void moveZeroes(vector<int>& nums)
+    {
         int n=nums.size();
+
         int count=0;
+
         for(int i=0;i<n;i++)
         {
-            if(nums[i]==0)  
+            if(nums[i]==0)
+            {
                 count++;
+            }
         }
+
         int idx=0;
+
         for(int i=0;i<n;i++)
         {
             if(nums[i]!=0)
@@ -17,7 +39,8 @@ public:
                 idx++;
             }
         }
-        for(int i=1;i<=count;i++)
+
+        while(count--)
         {
             nums[idx]=0;
             idx++;
