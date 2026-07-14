83733fcfd5f311bd2f2d847984f8fd2dc8cf0205
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-14 09:43:57 +0530

    Time: 3 ms (96.5%), Space: 33.3 MB (28.58%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,66 +1,69 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+// Approach
+// 1. Pehla sorted linked list nu
+//    vector ch convert karaange.
+// 2. Hun vector te Binary Search waali approach use karaange.
+// 3. Har recursive call ch
+//    middle element nu root banaavaange.
+// 4. Middle de left wale elements
+//    left subtree belong karange.
+// 5. Middle de right wale elements
+//    right subtree belong karange.
+// 6. Left te right parts lai
+//    recursively BST construct karaange.
+// 7. Base case:
+//    Je start > end hove,
+//    ta NULL return karaange.
+// 8. Recursion complete hon to baad
+//    balanced BST da root return karaange.
+
+class Solution
+{
 public:
     TreeNode* build_tree(vector<int>& arr,int start,int end)
     {
         if(start>end)
+        {
             return NULL;
+        }
 
         int mid;
-        if((end-start) %2==0)
-            mid=start+ (end-start)/2;
+
+        if((end-start)%2==0)
+        {
+            mid=start+(end-start)/2;
+        }
         else
-            mid=(start+ (end-start)/2)+1;
+        {
+            mid=start+(end-start)/2+1;
+        }
 
         TreeNode* root=new TreeNode(arr[mid]);
+
         root->left=build_tree(arr,start,mid-1);
+
         root->right=build_tree(arr,mid+1,end);
 
         return root;
     }
-    TreeNode* sortedListToBST(ListNode* head) {
+
+    TreeNode* sortedListToBST(ListNode* head)
+    {
         if(head==NULL)
         {
             return NULL;
         }
-        if(head->next==NULL)
-        {
-            TreeNode* root=new TreeNode(head->val);
-            return root;
-        }
 
-        vector<int>arr;
+        vector<int> arr;
 
         ListNode* temp=head;
+
         while(temp!=NULL)
         {
             arr.push_back(temp->val);
+
             temp=temp->next;
         }
 
-        int start=0;
-        int end=arr.size()-1;
-
-        return build_tree(arr,start,end);
+        return build_tree(arr,0,arr.size()-1);
     }
 };
