001d461df3678875d30318abb409f020d01954bb
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-14 09:32:06 +0530

    Time: 0 ms (100%), Space: 11.3 MB (39.44%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -1,81 +1,106 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
+// Approach
+// 1. Je left te right same hon
+//    ya linked list empty hove,
+//    ta original head return karaange.
+// 2. Je left == 1 hove,
+//    ta first right nodes reverse karaange.
+// 3. Reversed part de next nu
+//    remaining linked list naal connect karaange.
+// 4. Je left > 1 hove,
+//    ta pehla left-1 position tak move karaange,
+//    taaki reversed part ton pehla wala node mil jaave.
+// 5. Right position ton agla node
+//    (last part da first node) find karaange.
+// 6. Left te Right de vichale saare nodes reverse karaange,
+//    te reversed part da last node
+//    last part naal connect karaange.
+// 7. First part de last node nu
+//    reversed part de first node naal connect karaange.
+// 8. Final modified linked list da head return karaange.
+
+class Solution
+{
 public:
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
+    ListNode* reverseBetween(ListNode* head,int left,int right)
+    {
         if(head==NULL || head->next==NULL || left==right)
+        {
             return head;
+        }
 
-        
-
+        // Case 1 : Reverse starts from head
         if(left==1)
         {
-            int right1=right;
-            ListNode* temp=head;
-            ListNode* temp1=head;
+            int nodesToReverse=right;
+
+            ListNode* current=head;
+            ListNode* nextPartHead=head;
+
             while(right--)
             {
-                temp1=temp1->next;
-            }
-            ListNode* prev=temp1;
-            while(right1--)
-            {
-                ListNode* next_node=temp->next;
-                temp->next=prev;
-                prev=temp;
-                temp=next_node;
+                nextPartHead=nextPartHead->next;
             }
 
-            return prev;
+            ListNode* previous=nextPartHead;
+
+            while(nodesToReverse--)
+            {
+                ListNode* nextNode=current->next;
+
+                current->next=previous;
+
+                previous=current;
+
+                current=nextNode;
+            }
+
+            return previous;
         }
+
+        // Case 2 : Reverse starts after head
         else
         {
-            int left1=left;
-            int right1=right;
+            int originalLeft=left;
+            int originalRight=right;
 
-            left=left-2;
-            ListNode* first_part_last_node=head;
+            int moveSteps=left-2;
 
-            while(left--)
+            // Last node of first part
+            ListNode* firstPartLastNode=head;
+
+            while(moveSteps--)
             {
-                first_part_last_node=first_part_last_node->next;
+                firstPartLastNode=firstPartLastNode->next;
             }
 
-            ListNode* last_part_first_node=head;
-            while(right>0)
+            // First node of last part
+            ListNode* lastPartFirstNode=head;
+
+            while(right--)
             {
-                last_part_first_node=last_part_first_node->next;
-                right--;
+                lastPartFirstNode=lastPartFirstNode->next;
             }
 
-            ListNode* prev=last_part_first_node;
-            ListNode* middle_first=first_part_last_node->next;
-            int loop=right1-left1+1;
-            while(loop--)
-            {
-                ListNode* next_node=middle_first->next;
-                middle_first->next=prev;
-                prev=middle_first;
-                middle_first=next_node;
+            ListNode* previous=lastPartFirstNode;
 
+            ListNode* current=firstPartLastNode->next;
+
+            int nodesToReverse=originalRight-originalLeft+1;
+
+            while(nodesToReverse--)
+            {
+                ListNode* nextNode=current->next;
+
+                current->next=previous;
+
+                previous=current;
+
+                current=nextNode;
             }
 
-            first_part_last_node->next=prev;
+            firstPartLastNode->next=previous;
 
             return head;
-            
-
-
         }
-
-        
     }
 };
