900cee458ba53db3f44ecb60991f67f68b215fb0
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-16 09:22:18 +0530

    Time: 0 ms (100%), Space: 13.2 MB (86.94%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,20 +1,32 @@
-class Solution {
+// Approach
+// 1. Do variables (ones te twos) maintain karaange.
+// 2. Ones oh bits store karega
+//    jo exactly ik vaar appear hoye ne.
+// 3. Twos oh bits store karega
+//    jo exactly do vaar appear hoye ne.
+// 4. Jadon koi bit teesri vaar aundi hai,
+//    ta oh ones te twos dono ton remove ho jaandi hai.
+// 5. Is tarah har bit da count
+//    modulo 3 maintain hunda hai.
+// 6. Traversal complete hon to baad,
+//    ones ch sirf unique number di bits bachdiyan ne.
+// 7. Ones return karaange.
+
+class Solution
+{
 public:
-    int singleNumber(vector<int>& nums) {
-        int n=nums.size();
+    int singleNumber(vector<int>& nums)
+    {
+        int ones=0;
+        int twos=0;
 
-        unordered_map<int,int>m;
-        for(int i=0;i<n;i++)
+        for(int number:nums)
         {
-            m[nums[i]]++;
+            ones=(ones^number) & (~twos);
+
+            twos=(twos^number) & (~ones);
         }
 
-        for(auto it: m)
-        {
-            if(it.second==1)
-                return it.first;
-        }
-
-        return -1;
+        return ones;
     }
 };
