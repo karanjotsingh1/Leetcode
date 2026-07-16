c40ef2282cb1f9201ada66278af9f8eddbe2ce39
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-07-16 09:20:15 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.03%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,18 @@
-class Solution {
+// Approach
+// 1. First n odd numbers da sum hunda hai n².
+// 2. First n even numbers da sum hunda hai n(n+1).
+// 3. Required answer:
+//    gcd(n², n(n+1)).
+// 4. n common factor bahar kaddange:
+//    n × gcd(n, n+1).
+// 5. Consecutive numbers da gcd hamesha 1 hunda hai.
+// 6. Is karke final answer sirf n hovega.
+
+class Solution
+{
 public:
-    int gcdOfOddEvenSums(int n) {
+    int gcdOfOddEvenSums(int n)
+    {
         return n;
     }
 };
