b12a3bf964f4514d7f86c16e60eb324eb32e76ac
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-08-31 03:27:35 +0530

    Time: 0 ms (100%), Space: 118.1 MB (64.26%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,13 +1,13 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
@@ -16,14 +16,15 @@ public:
 
         ListNode* slow=head;
         ListNode* fast=head;
-        while(fast!=NULL && fast->next!=NULL)
+
+        while(fast && fast->next)
         {
             slow=slow->next;
             fast=fast->next->next;
         }
 
         ListNode* prev=NULL;
-        while(slow!=NULL)
+        while(slow)
         {
             ListNode* next_node=slow->next;
             slow->next=prev;
@@ -32,14 +33,98 @@ public:
         }
 
         fast=head;
-        while(fast!=NULL && prev!=NULL)
+        while(fast && prev)
         {
             if(fast->val!=prev->val)
                 return false;
 
-            prev=prev->next;
             fast=fast->next;
+            prev=prev->next;
         }
         return true;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Approach
+// // 1. Slow te Fast pointers use karke
+// //    linked list da middle node find karaange.
+// // 2. Slow ik step move karega,
+// //    jadki Fast do steps move karega.
+// // 3. Jadon Fast end te pahunch jaave,
+// //    Slow middle node te hovega.
+// // 4. Middle ton agge wali linked list
+// //    reverse karaange.
+// // 5. Ik pointer head ton
+// //    te ik pointer reversed second half ton
+// //    start karaange.
+// // 6. Dono halves de corresponding nodes compare karaange.
+// // 7. Je kise vi position te values different hon,
+// //    ta linked list palindrome nahi hai.
+// //    False return karaange.
+// // 8. Je saare nodes match kar jaan,
+// //    ta linked list palindrome hai.
+// //    True return karaange.
+
+// class Solution
+// {
+// public:
+//     bool isPalindrome(ListNode* head)
+//     {
+//         if(head==NULL || head->next==NULL)
+//         {
+//             return true;
+//         }
+
+//         // Find middle
+//         ListNode* slow=head;
+//         ListNode* fast=head;
+
+//         while(fast!=NULL && fast->next!=NULL)
+//         {
+//             slow=slow->next;
+//             fast=fast->next->next;
+//         }
+
+//         // Reverse second half
+//         ListNode* prev=NULL;
+
+//         while(slow!=NULL)
+//         {
+//             ListNode* next_node=slow->next;
+
+//             slow->next=prev;
+
+//             prev=slow;
+
+//             slow=next_node;
+//         }
+
+//         // Compare both halves
+//         fast=head;
+
+//         while(fast!=NULL && prev!=NULL)
+//         {
+//             if(fast->val!=prev->val)
+//             {
+//                 return false;
+//             }
+
+//             fast=fast->next;
+//             prev=prev->next;
+//         }
+
+//         return true;
+//     }
+// };
