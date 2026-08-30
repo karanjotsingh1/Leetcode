9d3380059047501c5fa0bf6be515e2acf929a602
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-08-30 18:01:55 +0530

    Time: 0 ms (100%), Space: 97.3 MB (87.86%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,43 +1,66 @@
-// Approach
-// 1. Ik variable maintain karaange
-//    jo sab ton sasti buying day (best_buy) nu store karega.
-// 2. Array nu left to right traverse karaange.
-// 3. Je current price best_buy ton chhoti hove,
-//    ta best_buy update karaange.
-// 4. Nahi ta current day te sell karan da profit calculate karaange.
-// 5. Maximum profit nu continuously update karaange.
-// 6. Traversal complete hon to baad
-//    maximum profit return karaange.
-// 7. Je koi profitable transaction possible na hove,
-//    ta answer 0 hi rahega.
-
-class Solution
-{
+class Solution {
 public:
-    int maxProfit(vector<int>& prices)
-    {
+    int maxProfit(vector<int>& prices) {
         int n=prices.size();
-
         if(n==1)
-        {
             return 0;
-        }
 
-        int best_buy=0;
         int max_profit=0;
-
+        int curr_buying_day=0;
         for(int i=1;i<n;i++)
         {
-            if(prices[i]<prices[best_buy])
+            if(prices[curr_buying_day] > prices[i])
             {
-                best_buy=i;
+                curr_buying_day=i;
             }
             else
             {
-                max_profit=max(max_profit,prices[i]-prices[best_buy]);
+                max_profit=max(prices[i]-prices[curr_buying_day],max_profit);
             }
         }
-
         return max_profit;
     }
 };
+// // Approach
+// // 1. Ik variable maintain karaange
+// //    jo sab ton sasti buying day (best_buy) nu store karega.
+// // 2. Array nu left to right traverse karaange.
+// // 3. Je current price best_buy ton chhoti hove,
+// //    ta best_buy update karaange.
+// // 4. Nahi ta current day te sell karan da profit calculate karaange.
+// // 5. Maximum profit nu continuously update karaange.
+// // 6. Traversal complete hon to baad
+// //    maximum profit return karaange.
+// // 7. Je koi profitable transaction possible na hove,
+// //    ta answer 0 hi rahega.
+
+// class Solution
+// {
+// public:
+//     int maxProfit(vector<int>& prices)
+//     {
+//         int n=prices.size();
+
+//         if(n==1)
+//         {
+//             return 0;
+//         }
+
+//         int best_buy=0;
+//         int max_profit=0;
+
+//         for(int i=1;i<n;i++)
+//         {
+//             if(prices[i]<prices[best_buy])
+//             {
+//                 best_buy=i;
+//             }
+//             else
+//             {
+//                 max_profit=max(max_profit,prices[i]-prices[best_buy]);
+//             }
+//         }
+
+//         return max_profit;
+//     }
+// };
