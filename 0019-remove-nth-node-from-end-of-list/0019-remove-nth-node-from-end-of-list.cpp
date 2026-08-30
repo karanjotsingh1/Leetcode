5edc31dd9d550ecbd4e1e18314a788a893015530
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-08-31 02:54:13 +0530

    Time: 0 ms (100%), Space: 14.9 MB (93.27%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,48 +1,98 @@
-// Approach
-// 1. Ik dummy node banaavaange,
-//    jo head ton pehla point karega.
-// 2. Do pointers (slow te fast)
-//    dummy node ton start karaange.
-// 3. Fast pointer nu n+1 steps agge move karaange.
-// 4. Hun slow te fast nu
-//    ik ik step move karaange.
-// 5. Jadon fast NULL ho jaave,
-//    slow remove hon wale node ton
-//    ik node pehla hovega.
-// 6. Slow da next pointer
-//    next node wal point kara devaange,
-//    jis naal nth node from end remove ho jaavegi.
-// 7. Dummy da next hi
-//    updated linked list da head hovega.
-
-class Solution
-{
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head,int n)
-    {
-        ListNode* dummy=new ListNode(-1);
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head->next==NULL)
+            return NULL;
 
-        dummy->next=head;
-
-        ListNode* slow=dummy;
-        ListNode* fast=dummy;
-
-        // Move fast n+1 steps ahead
-        for(int i=0;i<=n;i++)
+        int count=0;
+        ListNode* temp=head;
+        while(temp)
         {
-            fast=fast->next;
+            count+=1;
+            temp=temp->next;
+        }
+        if(n==count)
+        {
+            head=head->next;
+            return head;
+        }
+        int steps=count-n-1;
+
+        temp=head;
+        while(steps)
+        {
+            temp=temp->next;
+            steps--;
         }
 
-        // Move both pointers
-        while(fast!=NULL)
+        if(n==1)
         {
-            slow=slow->next;
-            fast=fast->next;
+            temp->next=NULL;
         }
-
-        // Remove nth node
-        slow->next=slow->next->next;
-
-        return dummy->next;
+        else
+        {
+            temp->next=temp->next->next;
+        }
+        return head;
     }
 };
+
+
+// // Approach
+// // 1. Ik dummy node banaavaange,
+// //    jo head ton pehla point karega.
+// // 2. Do pointers (slow te fast)
+// //    dummy node ton start karaange.
+// // 3. Fast pointer nu n+1 steps agge move karaange.
+// // 4. Hun slow te fast nu
+// //    ik ik step move karaange.
+// // 5. Jadon fast NULL ho jaave,
+// //    slow remove hon wale node ton
+// //    ik node pehla hovega.
+// // 6. Slow da next pointer
+// //    next node wal point kara devaange,
+// //    jis naal nth node from end remove ho jaavegi.
+// // 7. Dummy da next hi
+// //    updated linked list da head hovega.
+
+// class Solution
+// {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head,int n)
+//     {
+//         ListNode* dummy=new ListNode(-1);
+
+//         dummy->next=head;
+
+//         ListNode* slow=dummy;
+//         ListNode* fast=dummy;
+
+//         // Move fast n+1 steps ahead
+//         for(int i=0;i<=n;i++)
+//         {
+//             fast=fast->next;
+//         }
+
+//         // Move both pointers
+//         while(fast!=NULL)
+//         {
+//             slow=slow->next;
+//             fast=fast->next;
+//         }
+
+//         // Remove nth node
+//         slow->next=slow->next->next;
+
+//         return dummy->next;
+//     }
+// };
