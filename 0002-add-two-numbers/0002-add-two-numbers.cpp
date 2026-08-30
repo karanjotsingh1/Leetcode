af181f683fe40cc9428ffc0e11fa6227d372d010
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-08-31 03:02:29 +0530

    Time: 0 ms (100%), Space: 77.1 MB (46.02%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,50 +1,113 @@
-// Approach
-// 1. Pehlaa aapaa ik dummy node(ans) bnaavaange taaki answer list easily build ho sake.
-// 2. Ik temp pointer rakhange jo hamesha answer list de last node nu point kru ga.
-// 3. Ik carry variable rakhange jo previous addition da carry store kru ga.
-// 4. Hun jadon takk l1, l2 ya carry exist krde ne,
-//    ona nu ik ik karke add kraange.
-// 5. Sum da last digit navi node bna ke answer list ch add kr devaange,
-//    te carry update kr devaange.
-// 6. Jadon saari nodes process ho jaan,
-//    dummy node de next nu return kr devaange.
-
-class Solution
-{
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1,ListNode* l2)
-    {
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* temp1=l1;
+        ListNode* temp2=l2;
+
+        int carry=0;
         ListNode* ans=new ListNode(-1);
         ListNode* temp=ans;
 
-        int carry=0;
-        int sum=0;
-
-        while(l1!=NULL||l2!=NULL||carry)
+        while(temp1 || temp2 || carry)
         {
-            if(l1!=NULL)
+            int sum=0;
+
+            if(temp1)
             {
-                sum+=l1->val;
-                l1=l1->next;
+                sum+=temp1->val;
+                temp1=temp1->next;
+            }
+            if(temp2)
+            {
+                sum+=temp2->val;
+                temp2=temp2->next;
+            }
+            if(carry)
+            {
+                sum+=carry;
             }
 
-            if(l2!=NULL)
+            if(sum>=10)
             {
-                sum+=l2->val;
-                l2=l2->next;
+                carry=1;
+                sum=sum%10;
             }
-
-            sum+=carry;
-            carry=sum/10;
-
-            // Create a node for the current digit
-            ListNode* next_node=new ListNode(sum%10);
+            else
+            {
+                carry=0;
+            }
+            ListNode* next_node=new ListNode(sum);
             temp->next=next_node;
             temp=next_node;
-
-            sum=0;
         }
-
         return ans->next;
     }
 };
+
+
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+//         ListNode* temp1=l1;
+//         ListNode* temp2=l2;
+//         int carry=0;
+//         int sum=0;
+
+//         ListNode* ans=new ListNode(-1);
+//         ListNode* temp=ans;
+
+//         while(sum || temp1 || temp2)
+//         {
+//             if(temp1)
+//             {
+//                 sum+=temp1->val;
+//                 temp1=temp1->next;
+//             }
+//             if(temp2)
+//             {
+//                 sum+=temp2->val;
+//                 temp2=temp2->next;
+//             }
+//             if(carry)
+//             {
+//                 sum+=carry;
+//             }
+
+//             if(sum>=10)
+//             {
+//                 carry=1;
+//                 sum=sum%10;
+//             }
+//             else
+//             {
+//                 carry=0;
+//             }
+
+//             ListNode* new_node=new ListNode(sum);
+//             temp->next=new_node;
+//         }
+//     }
+// };
+
+
