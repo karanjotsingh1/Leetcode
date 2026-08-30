b2b42a6129c6530c5808fa0a6d1283299c544c32
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-08-31 03:37:03 +0530

    Time: 0 ms (100%), Space: 16.4 MB (63.87%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,51 +1,128 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
+
         if(head==NULL || head->next==NULL)
             return head;
-        
+            
         int count=0;
         ListNode* temp=head;
+
         while(temp)
         {
-            temp=temp->next;
             count+=1;
+            temp=temp->next;
         }
 
         k=k%count;
         if(k==0)
+        {
             return head;
-
-        int move_forward=count-k-1;
+        }
+        int steps=count-k-1;
 
         temp=head;
-        while(move_forward)
+        while(steps)
         {
             temp=temp->next;
-            move_forward--;
+            steps--;
         }
-        
-        ListNode* next_node=temp->next;
 
+        ListNode* new_head=temp->next;
         temp->next=NULL;
 
-        temp=next_node;
+        temp=new_head;
         while(temp->next)
         {
             temp=temp->next;
         }
         temp->next=head;
 
-        return next_node;
+        return new_head;
     }
 };
+
+
+
+// // Approach
+// // 1. Pehla linked list di length calculate karaange.
+// // 2. Rotation k nu
+// //    k % length kar devaange,
+// //    kyunki complete rotations da koi effect nahi hunda.
+// // 3. Je k == 0 hove,
+// //    ta original head return karaange.
+// // 4. New head ton ik node pehla wala node find karaange.
+// //    Isde lai (length-k-1) nodes agge move karaange.
+// // 5. New head nu store karaange.
+// // 6. Current node da next NULL kar devaange,
+// //    taaki list do parts ch divide ho jaave.
+// // 7. New head ton last node tak traverse karaange.
+// // 8. Last node nu original head naal connect karaange.
+// // 9. New head return karaange.
+
+// class Solution
+// {
+// public:
+//     ListNode* rotateRight(ListNode* head,int k)
+//     {
+//         if(head==NULL || head->next==NULL)
+//         {
+//             return head;
+//         }
+
+//         int count=0;
+
+//         ListNode* temp=head;
+
+//         // Find length
+//         while(temp!=NULL)
+//         {
+//             count++;
+//             temp=temp->next;
+//         }
+
+//         k=k%count;
+
+//         if(k==0)
+//         {
+//             return head;
+//         }
+
+//         // Move to node before new head
+//         int move_forward=count-k-1;
+
+//         temp=head;
+
+//         while(move_forward--)
+//         {
+//             temp=temp->next;
+//         }
+
+//         ListNode* new_head=temp->next;
+
+//         temp->next=NULL;
+
+//         // Reach last node
+//         temp=new_head;
+
+//         while(temp->next!=NULL)
+//         {
+//             temp=temp->next;
+//         }
+
+//         // Connect last node to old head
+//         temp->next=head;
+
+//         return new_head;
+//     }
+// };
