a70bc98bda980034b8ab31ef03059ce6d9cbbdf2
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-08-31 03:04:01 +0530

    Time: 7 ms (54.83%), Space: 12.5 MB (22.77%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -1,23 +1,39 @@
-// Approach
-// 1. Current node nu directly delete nhi kar sakde,
-//    kyunki previous node da access available nahi hai.
-// 2. Is karke next node di value current node ch copy karaange.
-// 3. Current node da next pointer nu
-//    next node de agge wale node wal point karaange.
-// 4. Is naal next node effectively list cho remove ho jaega.
-// 5. Last ch old next node di memory free kar devaange.
-
-class Solution
-{
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
 public:
-    void deleteNode(ListNode* node)
-    {
-        ListNode* next_node=node->next;
-
-        node->val=next_node->val;
-
-        node->next=next_node->next;
-
-        delete next_node;
+    void deleteNode(ListNode* node) {
+        node->val=node->next->val;
+        node->next=node->next->next;
     }
 };
+
+// // Approach
+// // 1. Current node nu directly delete nhi kar sakde,
+// //    kyunki previous node da access available nahi hai.
+// // 2. Is karke next node di value current node ch copy karaange.
+// // 3. Current node da next pointer nu
+// //    next node de agge wale node wal point karaange.
+// // 4. Is naal next node effectively list cho remove ho jaega.
+// // 5. Last ch old next node di memory free kar devaange.
+
+// class Solution
+// {
+// public:
+//     void deleteNode(ListNode* node)
+//     {
+//         ListNode* next_node=node->next;
+
+//         node->val=next_node->val;
+
+//         node->next=next_node->next;
+
+//         delete next_node;
+//     }
+// };
