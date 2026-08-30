029b7945d57280838f385ea8da64bb1ccde6fe1b
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-08-31 02:40:17 +0530

    Time: 0 ms (100%), Space: 13.5 MB (41.52%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,46 +1,78 @@
-// Approach
-// 1. Tin pointers use karaange:
-//    prev, temp te next_node.
-// 2. Prev nu NULL initialize karaange,
-//    kyunki reversed list da last node
-//    NULL nu point karega.
-// 3. Temp head ton traversal start karega.
-// 4. Current node de next pointer nu
-//    temporary variable (next_node) ch store karaange.
-// 5. Current node da next
-//    previous node wal point kara devaange.
-// 6. Prev nu current node te move karaange.
-// 7. Temp nu next_node te move karaange.
-// 8. Eh process jadon tak
-//    temp NULL na ho jaave,
-//    repeat karaange.
-// 9. Traversal complete hon to baad
-//    prev hi reversed linked list da new head hovega.
-
-class Solution
-{
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+class Solution {
 public:
-    ListNode* reverseList(ListNode* head)
-    {
-        if(head==NULL || head->next==NULL)
-        {
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL)
             return head;
-        }
 
-        ListNode* temp=head;
+        if(head->next==NULL)
+            return head;
+
         ListNode* prev=NULL;
-
-        while(temp!=NULL)
+        while(head!=NULL)
         {
-            ListNode* next_node=temp->next;
-
-            temp->next=prev;
-
-            prev=temp;
-
-            temp=next_node;
+            ListNode* next_node=head->next;
+            head->next=prev;
+            prev=head;
+            head=next_node;
         }
-
         return prev;
     }
 };
+
+
+// // Approach
+// // 1. Tin pointers use karaange:
+// //    prev, temp te next_node.
+// // 2. Prev nu NULL initialize karaange,
+// //    kyunki reversed list da last node
+// //    NULL nu point karega.
+// // 3. Temp head ton traversal start karega.
+// // 4. Current node de next pointer nu
+// //    temporary variable (next_node) ch store karaange.
+// // 5. Current node da next
+// //    previous node wal point kara devaange.
+// // 6. Prev nu current node te move karaange.
+// // 7. Temp nu next_node te move karaange.
+// // 8. Eh process jadon tak
+// //    temp NULL na ho jaave,
+// //    repeat karaange.
+// // 9. Traversal complete hon to baad
+// //    prev hi reversed linked list da new head hovega.
+
+// class Solution
+// {
+// public:
+//     ListNode* reverseList(ListNode* head)
+//     {
+//         if(head==NULL || head->next==NULL)
+//         {
+//             return head;
+//         }
+
+//         ListNode* temp=head;
+//         ListNode* prev=NULL;
+
+//         while(temp!=NULL)
+//         {
+//             ListNode* next_node=temp->next;
+
+//             temp->next=prev;
+
+//             prev=temp;
+
+//             temp=next_node;
+//         }
+
+//         return prev;
+//     }
+// };
