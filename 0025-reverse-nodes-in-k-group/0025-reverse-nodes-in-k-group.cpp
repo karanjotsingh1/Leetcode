b8478cee64e63a5fb43c6214e783665a8a147696
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-08-31 03:22:40 +0530

    Time: 0 ms (100%), Space: 16.6 MB (39.21%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,58 +1,106 @@
-// Approach
-// 1. Pehlaa check kraange ki current group ch k nodes present ne ya nahi.
-//    Je k nodes nhi ne, ta current node hi return kr devaange,
-//    kyunki last incomplete group nu reverse nhi krna.
-// 2. Je k nodes present ne,
-//    ta remaining list lai recursively function call kraange.
-// 3. Recursive call sanu agle groups da reversed head return kru gi.
-// 4. Hun current k nodes nu reverse kraange
-//    te ona da last node recursively returned list naal connect kr devaange.
-// 5. Reverse complete hon to baad current group da nava head return kr devaange.
-// 6. Is process naal saare complete groups reverse ho jaan ge,
-//    te last incomplete group unchanged hi reh jaega.
-
-class Solution
-{
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+class Solution {
 public:
-    ListNode* get_head(ListNode* node,int k)
+    ListNode* Reverse_groups(ListNode* head, int k)
     {
-        ListNode* temp=node;
-        int cnt=0;
-
-        // Check if current group has k nodes
-        while(cnt<k&&temp!=NULL)
+        ListNode* temp=head;
+        int kk=k;
+        while(kk)
         {
+            if(temp==NULL)
+                return head;
+
             temp=temp->next;
-            cnt++;
+            kk=kk-1;
         }
 
-        if(cnt<k)
-        {
-            return node;
-        }
+        ListNode* prev=Reverse_groups(temp,k);
 
-        // Reverse the remaining groups first
-        ListNode* prev=get_head(temp,k);
-
-        // Reverse current group
-        while(cnt--)
+        temp=head;
+        kk=k;
+        while(kk)
         {
-            ListNode* next_node=node->next;
-            node->next=prev;
-            prev=node;
-            node=next_node;
+            ListNode* next_node=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=next_node;
+            kk--;
         }
 
         return prev;
     }
-
-    ListNode* reverseKGroup(ListNode* head,int k)
-    {
-        if(head==NULL)
-        {
-            return NULL;
-        }
-
-        return get_head(head,k);
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* ans=Reverse_groups(head,k);
+        return ans;
     }
 };
+
+
+
+// // Approach
+// // 1. Pehlaa check kraange ki current group ch k nodes present ne ya nahi.
+// //    Je k nodes nhi ne, ta current node hi return kr devaange,
+// //    kyunki last incomplete group nu reverse nhi krna.
+// // 2. Je k nodes present ne,
+// //    ta remaining list lai recursively function call kraange.
+// // 3. Recursive call sanu agle groups da reversed head return kru gi.
+// // 4. Hun current k nodes nu reverse kraange
+// //    te ona da last node recursively returned list naal connect kr devaange.
+// // 5. Reverse complete hon to baad current group da nava head return kr devaange.
+// // 6. Is process naal saare complete groups reverse ho jaan ge,
+// //    te last incomplete group unchanged hi reh jaega.
+
+// class Solution
+// {
+// public:
+//     ListNode* get_head(ListNode* node,int k)
+//     {
+//         ListNode* temp=node;
+//         int cnt=0;
+
+//         // Check if current group has k nodes
+//         while(cnt<k && temp!=NULL)
+//         {
+//             temp=temp->next;
+//             cnt++;
+//         }
+
+//         if(cnt<k)
+//         {
+//             return node;
+//         }
+
+//         // Reverse the remaining groups first
+//         ListNode* prev=get_head(temp,k);
+
+//         // Reverse current group
+//         while(cnt--)
+//         {
+//             ListNode* next_node=node->next;
+//             node->next=prev;
+//             prev=node;
+//             node=next_node;
+//         }
+
+//         return prev;
+//     }
+
+//     ListNode* reverseKGroup(ListNode* head,int k)
+//     {
+//         if(head==NULL)
+//         {
+//             return NULL;
+//         }
+
+//         return get_head(head,k);
+//     }
+// };
