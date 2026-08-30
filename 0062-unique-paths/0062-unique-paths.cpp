784c49ee3e45ab9adfe0b3df58157e4b6fefa8d5
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-08-31 02:33:59 +0530

    Time: 0 ms (100%), Space: 8.4 MB (77.82%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-
     int uniquePaths(int m, int n) {
-        // Stores the total number of unique paths
-        long long paths = 1;
+        
+        vector<int> dp(n,1);
 
-        // Total moves needed to reach the destination
-        int totalMoves = m + n - 2;
-
-        // Compute C(totalMoves, r), where r is smaller
-        // because C(n, r) = C(n, n - r)
-        int r = min(m - 1, n - 1);
-
-        // Calculate the binomial coefficient iteratively
-        for (int i = 1; i <= r; i++)
+        for(int i=1;i<m;i++)
         {
-            paths = paths * (totalMoves - r + i) / i;
+            for(int j=1;j<n;j++)
+            {
+                dp[j]=dp[j]+dp[j-1];
+            }
         }
 
-        return (int)paths;
-    
+        return dp[n-1];
     }
 };
