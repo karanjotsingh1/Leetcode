b7c3fa08bcfbd0b818f8d785f84309965b787a97
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-08-31 02:21:20 +0530

    Time: 0 ms (100%), Space: 28.2 MB (75.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        
         int n=nums.size();
-
         if(n==1)
+        {
             return nums[0];
+        }
 
-        int majority=nums[0];
+        int majority_el=nums[0];
         int freq=1;
         for(int i=1;i<n;i++)
         {
-            if(nums[i]==majority)
+            if(nums[i]==majority_el)
             {
                 freq++;
             }
@@ -19,11 +21,43 @@ public:
                 freq--;
                 if(freq==0)
                 {
-                    majority=nums[i];
+                    majority_el=nums[i];
                     freq=1;
                 }
             }
         }
-        return majority;
+        return majority_el;
+
     }
 };
+
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n=nums.size();
+
+//         if(n==1)
+//             return nums[0];
+
+//         int majority=nums[0];
+//         int freq=1;
+//         for(int i=1;i<n;i++)
+//         {
+//             if(nums[i]==majority)
+//             {
+//                 freq++;
+//             }
+//             else
+//             {
+//                 freq--;
+//                 if(freq==0)
+//                 {
+//                     majority=nums[i];
+//                     freq=1;
+//                 }
+//             }
+//         }
+//         return majority;
+//     }
+// };
