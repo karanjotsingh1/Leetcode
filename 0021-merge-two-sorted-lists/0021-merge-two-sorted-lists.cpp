afecf17ad37ae66f0ea29c45ee4504e661be58fc
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-08-31 02:49:08 +0530

    Time: 0 ms (100%), Space: 19.7 MB (8.44%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,59 +1,123 @@
-// Approach
-// 1. Ik dummy node banaavaange,
-//    jo merged list da starting point hovega.
-// 2. Ik pointer (temp) maintain karaange,
-//    jo merged list da last node represent karega.
-// 3. Dono linked lists nu simultaneously traverse karaange.
-// 4. Je list1 da current node chhota ya equal hove,
-//    ta us node nu merged list ch attach karaange
-//    te list1 nu agge move karaange.
-// 5. Nahi ta list2 da current node attach karaange
-//    te list2 nu agge move karaange.
-// 6. Har attachment ton baad
-//    temp nu agge move karaange.
-// 7. Jadon kise ik list de nodes khatam ho jaan,
-//    ta doosri list de saare remaining nodes
-//    directly merged list ch attach kar devaange.
-// 8. Dummy node de next nu return karaange,
-//    jo merged sorted list da head hovega.
-
-class Solution
-{
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1,ListNode* list2)
-    {
-        ListNode* head=new ListNode(-1);
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* ans=new ListNode(-1);
+        ListNode* temp=ans;
 
-        ListNode* temp=head;
-
-        while(list1!=NULL && list2!=NULL)
+        while(list1 && list2)
         {
-            if(list1->val<=list2->val)
+            if(list1->val < list2->val)
             {
-                temp->next=list1;
-
+                ListNode* next_node=new ListNode(list1->val);
+                temp->next=next_node;
+                temp=next_node;
                 list1=list1->next;
+            }
+            else if(list1->val > list2->val)
+            {
+                ListNode* next_node=new ListNode(list2->val);
+                temp->next=next_node;
+                temp=next_node;
+                list2=list2->next;
             }
             else
             {
-                temp->next=list2;
-
+                ListNode* next_node1=new ListNode(list1->val);
+                temp->next=next_node1;
+                temp=next_node1;
+                list1=list1->next;
+                ListNode* next_node2=new ListNode(list2->val);
+                temp->next=next_node2;
+                temp=next_node2;
                 list2=list2->next;
             }
-
-            temp=temp->next;
         }
-
-        if(list1!=NULL)
+        while(list1)
         {
-            temp->next=list1;
+             ListNode* next_node=new ListNode(list1->val);
+            temp->next=next_node;
+            temp=next_node;
+            list1=list1->next;
         }
-
-        if(list2!=NULL)
+        while(list2)
         {
-            temp->next=list2;
+            ListNode* next_node=new ListNode(list2->val);
+            temp->next=next_node;
+            temp=next_node;
+            list2=list2->next;
         }
-
-        return head->next;
+        return ans->next;
     }
 };
+
+
+
+// // Approach
+// // 1. Ik dummy node banaavaange,
+// //    jo merged list da starting point hovega.
+// // 2. Ik pointer (temp) maintain karaange,
+// //    jo merged list da last node represent karega.
+// // 3. Dono linked lists nu simultaneously traverse karaange.
+// // 4. Je list1 da current node chhota ya equal hove,
+// //    ta us node nu merged list ch attach karaange
+// //    te list1 nu agge move karaange.
+// // 5. Nahi ta list2 da current node attach karaange
+// //    te list2 nu agge move karaange.
+// // 6. Har attachment ton baad
+// //    temp nu agge move karaange.
+// // 7. Jadon kise ik list de nodes khatam ho jaan,
+// //    ta doosri list de saare remaining nodes
+// //    directly merged list ch attach kar devaange.
+// // 8. Dummy node de next nu return karaange,
+// //    jo merged sorted list da head hovega.
+
+// class Solution
+// {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1,ListNode* list2)
+//     {
+//         ListNode* head=new ListNode(-1);
+
+//         ListNode* temp=head;
+
+//         while(list1!=NULL && list2!=NULL)
+//         {
+//             if(list1->val<=list2->val)
+//             {
+//                 temp->next=list1;
+
+//                 list1=list1->next;
+//             }
+//             else
+//             {
+//                 temp->next=list2;
+
+//                 list2=list2->next;
+//             }
+
+//             temp=temp->next;
+//         }
+
+//         if(list1!=NULL)
+//         {
+//             temp->next=list1;
+//         }
+
+//         if(list2!=NULL)
+//         {
+//             temp->next=list2;
+//         }
+
+//         return head->next;
+//     }
+// };
