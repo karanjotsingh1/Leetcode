8d0094e8fcdef0da9f88e230c35465721a1eac68
karanjotsingh1 <karanjotsingh18400@gmail.com>
2026-09-01 19:02:10 +0530

    Time: 40 ms (27.76%), Space: 97 MB (16.64%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,22 @@
+class StockSpanner {
+    stack<pair<int,int>> st;
+
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span=1;
+
+        while(st.empty()==false && st.top().first<=price)
+        {
+            span+=st.top().second;
+            st.pop();
+        }
+
+        st.push({price,span});
+
+        return span;
+    }
+};
